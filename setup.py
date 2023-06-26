fd4689aa5b53936d93cada3d524ce3b62f9e76c6
Kye <kye@apacmediasolutions.com>
2023-06-26 18:18:05 -0400

    setup py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'swarms',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Swarms - Pytorch',
+  author = 'Kye Gomez',
+  author_email = 'kye@apac.ai',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/kyegomez/swarms',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'optimizers',
+    "Prompt Engineering"
+  ],
+  install_requires=[
+    'guidance',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
