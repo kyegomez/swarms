5580ae3eb3f19e36035c049252801a4ec3d80453
Kye <kye@apacmediasolutions.com>
2023-06-30 15:27:31 -0400

    clean up for init of main boss and worker classes

--- worker_example.py
+++ worker_example.py
@@ -1,14 +1,7 @@
-from swarms import WorkerNode, tools, vectorstore, llm
-#define tools (assuming process csv)
+from swarms import worker_node
 
-
-
-#inti worker node with llm
-worker_node = WorkerNode(llm=llm, tools=tools, vectorstore=vectorstore)
-
-#create an agent within the worker node
-worker_node.create_agent(ai_name="AI Assistant", ai_role="Assistant", human_in_the_loop=True, search_kwargs={})
+#cretae an agent 
+worker_node.create_agent(ai_name="Workerx", ai_role="Assistant", human_in_the_loop=True, search_kwargs={"k": 8})
 
 #use the agent to perform a task
 worker_node.run_agent("Find 20 potential customers for a Swarms based AI Agent automation infrastructure")
-
