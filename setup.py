cc0617111073b9ac01aa09bb8c87ccb41d97bb86
Kye <kye@apacmediasolutions.com>
2023-07-03 22:51:31 -0400

    clean up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'swarms',
   packages = find_packages(exclude=[]),
-  version = '0.2.4',
+  version = '0.2.5',
   license='MIT',
   description = 'Swarms - Pytorch',
   author = 'Kye Gomez',
@@ -26,7 +26,7 @@ setup(
         "nest_asyncio",
         "bs4",
         "playwright",
-        'git+https://github.com/facebookresearch/segment-anything.git',
+        'https://github.com/facebookresearch/segment-anything.git',
         "duckduckgo_search",
         "faiss-cpu",
         "wget==3.2",
