6e82b71c3aa895f18a4ed990b0a0458af400216e
Kye <kye@apacmediasolutions.com>
2023-07-03 23:36:10 -0400

    imports

--- swarms/agents/workers/multi_modal.py
+++ swarms/agents/workers/multi_modal.py
@@ -33,13 +33,13 @@ from langchain.llms.openai import OpenAI
 
 # Grounding DINO
 # import groundingdino.datasets.transforms as T
-import models.GroundingDINO.groundingdino.datasets.transforms as T
-from models.GroundingDINO.groundingdino.models import build_model
-from models.GroundingDINO.groundingdino.util import box_ops
-from models.GroundingDINO.groundingdino.util.slconfig import SLConfig
-from models.GroundingDINO.groundingdino.util.utils import clean_state_dict, get_phrases_from_posmap
+import swarms.agents.workers.models.GroundingDINO.groundingdino.datasets.transforms as T
+from swarms.agents.workers.models.GroundingDINO.groundingdino.models import build_model
+from swarms.agents.workers.models.GroundingDINO.groundingdino.util import box_ops
+from swarms.agents.workers.models.GroundingDINO.groundingdino.util.slconfig import SLConfig
+from swarms.agents.workers.models.GroundingDINO.groundingdino.util.utils import clean_state_dict, get_phrases_from_posmap
 
-from models.segment_anything.segment_anything import build_sam, SamPredictor, SamAutomaticMaskGenerator
+from swarms.agents.workers.models.segment_anything.segment_anything import build_sam, SamPredictor, SamAutomaticMaskGenerator
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
