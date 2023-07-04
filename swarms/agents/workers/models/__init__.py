5131f2b281ca21e8269e601c72ac29c75bca5a30
Kye <kye@apacmediasolutions.com>
2023-07-04 00:28:06 -0400

    relative imports

--- swarms/agents/workers/models/__init__.py
+++ swarms/agents/workers/models/__init__.py
@@ -1,6 +1,5 @@
-import swarms.agents.workers.models.GroundingDINO.groundingdino.datasets.transforms as T
-from swarms.agents.workers.models.GroundingDINO.groundingdino.models import build_model
-from swarms.agents.workers.models.GroundingDINO.groundingdino.util import box_ops
-from swarms.agents.workers.models.GroundingDINO.groundingdino.util.slconfig import SLConfig
-from swarms.agents.workers.models.GroundingDINO.groundingdino.util.utils import clean_state_dict, get_phrases_from_posmap
-from swarms.agents.workers.models.segment_anything.segment_anything import build_sam, SamPredictor, SamAutomaticMaskGenerator
+from .GroundingDINO.groundingdino.datasets.transforms import T
+from .GroundingDINO.groundingdino.models import build_model
+from .GroundingDINO.groundingdino.util import box_ops, SLConfig
+from .GroundingDINO.groundingdino.util.utils import clean_state_dict, get_phrases_from_posmap
+from .segment_anything.segment_anything import build_sam, SamPredictor, SamAutomaticMaskGenerator
