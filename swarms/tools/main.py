ca3b30f2500590f4da9833c7bf5b246779678ffc
Kye <kye@apacmediasolutions.com>
2023-07-04 15:29:12 -0400

    clean up

--- swarms/tools/main.py
+++ swarms/tools/main.py
@@ -435,8 +435,7 @@ import time
 from datetime import datetime
 from typing import Dict, List
 
-from swarms.utils import ANSI, Color, Style # test
-
+from swarms.utils.utils import ANSI, Color, Style # test
 
 class Terminal(BaseToolSet):
     def __init__(self):
