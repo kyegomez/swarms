9ef17e598ac855951933a30d421bf6b6add48d76
Kye <kye@apacmediasolutions.com>
2023-07-03 20:21:11 -0400

    verison 0.1.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'swarms',
   packages = find_packages(exclude=[]),
-  version = '0.1.5',
+  version = '0.1.6',
   license='MIT',
   description = 'Swarms - Pytorch',
   author = 'Kye Gomez',
