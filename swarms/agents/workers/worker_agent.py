bd41be51b07d9ddd71105e24c5123fa3392495d0
Kye <kye@apacmediasolutions.com>
2023-07-05 18:03:27 -0400

    clean up

--- swarms/agents/workers/worker_agent.py
+++ swarms/agents/workers/worker_agent.py
@@ -13,9 +13,6 @@ class WorkerNode(BaseTool):
     """Useful for when you need to spawn an autonomous agent instance as a worker to accomplish complex tasks, it can search the internet or spawn child multi-modality models to process and generate images and text or audio and so on """
     name = "WorkerNode"
     description = "A worker node that can perform complex tasks"
-    llm: Any = Field(description="Language model")
-    tools: List[Tool] = Field(description="List of tools")
-    vectorstore: VectorStore = Field(description="Vector store")
 
     def __init__(self, llm, tools, vectorstore):
         super().__init__(llm=llm, tools=tools, vectorstore=vectorstore)
