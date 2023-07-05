55e07625a67d3dccc2d12932b8b0fba5c28e8c7f
Kye <kye@apacmediasolutions.com>
2023-07-05 15:42:01 -0400

    clean up

--- swarms/swarms.py
+++ swarms/swarms.py
@@ -44,7 +44,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain, BaseCombineDocumentsChain
 
 from langchain.tools.human.tool import HumanInputRun
-from swarms.tools import Terminal, CodeWriter, CodeEditor, process_csv, WebpageQATool, math_tool
+from swarms.tools import Terminal, CodeWriter, CodeEditor, process_csv, WebpageQATool
 
 
 openai_api_key = os.environ["OPENAI_API_KEY"]
