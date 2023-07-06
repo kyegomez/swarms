b14aee130babf96104c5a592a7aff2fca9934e6b
Kye <kye@apacmediasolutions.com>
2023-07-06 14:23:06 -0400

    clean up for boss node

--- swarms/agents/boss/boss_agent.py
+++ swarms/agents/boss/boss_agent.py
@@ -7,14 +7,14 @@ class BossNode:
         self.llm = llm
         self.vectorstore = vectorstore
         self.agent_executor = agent_executor
-        # self.max_iterations = max_iterations
+        self.max_iterations = max_iterations
 
         try:
             self.baby_agi = BabyAGI.from_llm(
                 llm=self.llm,
                 vectorstore=self.vectorstore,
                 task_execution_chain=self.agent_executor,
-                # max_iterations=self.max_iterations,
+                max_iterations=self.max_iterations,
             )
         except ValidationError as e:
             print(f"Validation Error while initializing BabyAGI: {e}")
