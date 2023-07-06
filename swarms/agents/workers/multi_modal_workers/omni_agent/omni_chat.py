4173e300b2993739f4579843dd73f72caf2cdddf
Kye <kye@apacmediasolutions.com>
2023-07-05 22:49:44 -0400

    omni agent clean up config

--- swarms/agents/workers/multi_modal_workers/omni_agent/omni_chat.py
+++ swarms/agents/workers/multi_modal_workers/omni_agent/omni_chat.py
@@ -27,12 +27,12 @@ from huggingface_hub.inference_api import InferenceApi
 from huggingface_hub.inference_api import ALL_TASKS
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--config", type=str, default="configs/config.default.yaml")
+parser.add_argument("--config", type=str, default="config.yml")
 parser.add_argument("--mode", type=str, default="cli")
 args = parser.parse_args()
 
 if __name__ != "__main__":
-    args.config = "configs/config.gradio.yaml"
+    args.config = "config.yml"
     args.mode = "gradio"
 
 config = yaml.load(open(args.config, "r"), Loader=yaml.FullLoader)
