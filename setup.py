1f4d5f5c30d4ef0966d6040b5aa50cf4c6a43a68
Kye <kye@apacmediasolutions.com>
2023-07-06 14:29:25 -0400

    IT WORKS THIS IS INSANE

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'swarms',
   packages = find_packages(exclude=[]),
-  version = '0.6.3',
+  version = '0.6.4',
   license='MIT',
   description = 'Swarms - Pytorch',
   author = 'Kye Gomez',
