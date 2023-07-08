fd7bf3d0d8f085c3386e0cd54978f3be1076e1a0
Kye <kye@apacmediasolutions.com>
2023-07-08 17:33:06 -0400

    clean up v

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'swarms',
   packages = find_packages(exclude=[]),
-  version = '0.7.9',
+  version = '0.8.0',
   license='MIT',
   description = 'Swarms - Pytorch',
   author = 'Kye Gomez',
