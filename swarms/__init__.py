f0a827f7a2958203636567080c6efc916034d6e3
Kye <kye@apacmediasolutions.com>
2023-07-10 22:55:10 -0400

    ultra node
    Former-commit-id: 8dc55fef320880938270827c39bd4272f3b802c8

--- swarms/__init__.py
+++ swarms/__init__.py
@@ -1,4 +1,3 @@
 # from swarms import Swarms, swarm
 from swarms.swarms import Swarms, swarm
-from swarms.agents import worker_node
-from swarms.agents.workers.worker_ultranode import UltraNode
+from swarms.agents import worker_node, UltraNode
