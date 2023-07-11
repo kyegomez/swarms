737cc8a875d400ebb95247d40662bc6fef5ead18
Kye <kye@apacmediasolutions.com>
2023-07-10 21:59:00 -0400

    ultra node
    Former-commit-id: 60adc3484e70b3dbdfa170ee6c4c3a02b4c60644

---
+++ playground/ultranode_example.py
@@ -0,0 +1,18 @@
+from swarms import UltraNode
+
+# Define an objective
+objective = """
+Please make a web GUI for using HTTP API server. 
+The name of it is Swarms. 
+You can check the server code at ./main.py. 
+The server is served on localhost:8000. 
+Users should be able to write text input as 'query' and url array as 'files', and check the response. 
+Users input form should be delivered in JSON format. 
+I want it to have neumorphism-style. Serve it on port 4500.
+
+"""
+
+node = UltraNode(objective)
+
+
+result = node.execute()
