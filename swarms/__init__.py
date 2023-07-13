38600ef414ac646d1b5baba99581204cc6bb2441
Kye <kye@apacmediasolutions.com>
2023-07-12 21:28:10 -0400

    worker ultra node

--- swarms/__init__.py
+++ swarms/__init__.py
@@ -1,3 +1,4 @@
 # from swarms import Swarms, swarm
 from swarms.swarms import Swarms, swarm
-from swarms.agents.workers.WorkerUltraNode import worker_node, WorkerUltra
+from swarms.agents import worker_node
+from swarms.agents.workers.WorkerUltraNode import WorkerUltra
