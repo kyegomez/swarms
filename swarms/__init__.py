78dc1ab8dbf6fd754a940b761fe9b4487e4be189
Kye <kye@apacmediasolutions.com>
2023-07-12 22:15:23 -0400

    clean up
    Former-commit-id: 302716b676cd4f65b15b51f71a6463411d9db6cf

--- swarms/__init__.py
+++ swarms/__init__.py
@@ -1,4 +1,4 @@
 # from swarms import Swarms, swarm
 from swarms.swarms import Swarms, swarm
 from swarms.agents import worker_node
-from swarms.agents.workers.WorkerUltraNode import WorkerUltraNode
+from swarms.agents.workers.WorkerUltraNode import WorkerUltra
