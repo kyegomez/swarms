12e6fb218cb4dddde3875120a472f044bb2f4d62
Kye <kye@apacmediasolutions.com>
2023-07-14 21:53:59 -0400

    logging level debug
    Former-commit-id: 7ff2fd820b081e53939b02c4b1092bb69dba24af

--- swarms/agents/boss/BossNode.py
+++ swarms/agents/boss/BossNode.py
@@ -2,7 +2,7 @@ from swarms.tools.agent_tools import *
 from pydantic import ValidationError
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # ---------- Boss Node ----------
 class BossNode:
