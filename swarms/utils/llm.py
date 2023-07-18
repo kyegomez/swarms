40a200c924a3aef4c431e31546dde9a1172aa1d6
Kye <kye@apacmediasolutions.com>
2023-07-18 11:04:29 -0400

    clean up
    Former-commit-id: 70078d8845ab5af847bffbe5192fad563ffabb3e

--- swarms/utils/llm.py
+++ swarms/utils/llm.py
@@ -52,13 +52,13 @@ class LLM:
             logger.error("Failed to generate response: %s", e)
             raise
 
-# example
-from swarms.utils.llm import LLM
-llm_instance = LLM(openai_api_key="your_openai_key")
-result = llm_instance.run("Who won the FIFA World Cup in 1998?")
-print(result)
+# # example
+# from swarms.utils.llm import LLM
+# llm_instance = LLM(openai_api_key="your_openai_key")
+# result = llm_instance.run("Who won the FIFA World Cup in 1998?")
+# print(result)
 
-# using HuggingFaceHub
-llm_instance = LLM(hf_repo_id="google/flan-t5-xl", hf_api_token="your_hf_api_token")
-result = llm_instance.run("Who won the FIFA World Cup in 1998?")
-print(result)
+# # using HuggingFaceHub
+# llm_instance = LLM(hf_repo_id="google/flan-t5-xl", hf_api_token="your_hf_api_token")
+# result = llm_instance.run("Who won the FIFA World Cup in 1998?")
+# print(result)
