60eab358d401780b1e713235cada57cf4b2bfb9e
Kye <kye@apacmediasolutions.com>
2023-07-21 19:35:16 -0400

    clean up
    Former-commit-id: a6f815d01ced8337a901620b4061b23778edda11

--- swarms/agents/utils/ConversationalChatAgent.py
+++ swarms/agents/utils/ConversationalChatAgent.py
@@ -26,7 +26,7 @@ from langchain.agents.agent import AgentOutputParser
 from langchain.schema import AgentAction
 
 
-from swarms.agents.utils.prompts import EVAL_TOOL_RESPONSE
+from swarms.agents.prompts.prompts import EVAL_TOOL_RESPONSE
 
 
 
