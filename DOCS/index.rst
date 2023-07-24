36a46f71a0da131e143d50cba2f6e6511d4d76d1
Kye <kye@apacmediasolutions.com>
2023-07-24 00:37:12 -0400

    clean up documentation

---
+++ DOCS/index.rst
@@ -0,0 +1,20 @@
+.. swarms documentation master file, created by
+   sphinx-quickstart on Mon Jul 24 00:33:43 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to swarms's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ swarms/agents/models/petals_hf.py
@@ -0,0 +1,11 @@
+from transformers import AutoTokenizer
+from petals import AutoDistributedForCasualLM
+
+class PetalsHFLLM:
+    def __init__(self, model_name: str = None, prompt: str = None, device: str = None, use_fast = False, add_bos_token: str = None, cuda=False):
+        self.model_name = model_name
+        self.prompt = prompt 
+        self.device = device
+        self.use_fast = use_fast
+        self.add_bos_token = add_bos_token
+        self.cuda = cuda
