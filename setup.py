063a878b5a3f1ecb13a4b6451276bb2695dd3a0a
Kye <kye@apacmediasolutions.com>
2023-07-27 20:48:04 -0400

    clena up

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'swarms',
   packages = find_packages(exclude=[]),
-  version = '1.0.9',
+  version = '1.1.0',
   license='MIT',
   description = 'Swarms - Pytorch',
   author = 'Kye Gomez',
