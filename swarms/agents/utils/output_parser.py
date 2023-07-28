3929b43e69fa5ed9cd8cbeca3285cd1ed94d6556
Kye <kye@apacmediasolutions.com>
2023-07-28 13:02:18 -0400

    base agent class
    Former-commit-id: a1f119d85d9d9329b3e12e2be4a233800740fb7d

--- swarms/agents/utils/output_parser.py
+++ swarms/agents/utils/output_parser.py
@@ -1,9 +1,12 @@
+import json
 import re
-from typing import Dict
+from abc import abstractmethod
+from typing import Dict, NamedTuple
 
 from langchain.schema import BaseOutputParser
 
 from swarms.agents.prompts.prompts import EVAL_FORMAT_INSTRUCTIONS
+
 
 class EvalOutputParser(BaseOutputParser):
     @staticmethod
@@ -40,3 +43,66 @@ class EvalOutputParser(BaseOutputParser):
 
     def __str__(self):
         return "EvalOutputParser"
+    
+
+
+class AgentAction(NamedTuple):
+    """Action for Agent."""
+
+    name: str
+    """Name of the action."""
+    args: Dict
+    """Arguments for the action."""
+
+
+class BaseAgentOutputParser(BaseOutputParser):
+    """Base class for Agent output parsers."""
+
+    @abstractmethod
+    def parse(self, text: str) -> AgentAction:
+        """Parse text and return AgentAction"""
+
+
+def preprocess_json_input(input_str: str) -> str:
+    """Preprocesses a string to be parsed as json.
+
+    Replace single backslashes with double backslashes,
+    while leaving already escaped ones intact.
+
+    Args:
+        input_str: String to be preprocessed
+
+    Returns:
+        Preprocessed string
+    """
+    corrected_str = re.sub(
+        r'(?<!\\)\\(?!["\\/bfnrt]|u[0-9a-fA-F]{4})', r"\\\\", input_str
+    )
+    return corrected_str
+
+
+class AgentOutputParser(BaseAgentOutputParser):
+    """Output parser for Agent."""
+
+    def parse(self, text: str) -> AgentAction:
+        try:
+            parsed = json.loads(text, strict=False)
+        except json.JSONDecodeError:
+            preprocessed_text = preprocess_json_input(text)
+            try:
+                parsed = json.loads(preprocessed_text, strict=False)
+            except Exception:
+                return AgentAction(
+                    name="ERROR",
+                    args={"error": f"Could not parse invalid json: {text}"},
+                )
+        try:
+            return AgentAction(
+                name=parsed["command"]["name"],
+                args=parsed["command"]["args"],
+            )
+        except (KeyError, TypeError):
+            # If the command is null or incomplete, return an erroneous tool
+            return AgentAction(
+                name="ERROR", args={"error": f"Incomplete command args: {parsed}"}
+            )
