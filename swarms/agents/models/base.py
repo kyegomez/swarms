874eaa12e1439a7713ac8521acda895a7a719afb
Kye <kye@apacmediasolutions.com>
2023-07-30 09:08:44 -0400

    iteratisons on agent class, moving away from langchain

--- swarms/agents/models/base.py
+++ swarms/agents/models/base.py
@@ -2,11 +2,6 @@ from abc import ABC, abstractmethod
 
 class AbstractModel(ABC):
     #abstract base class for language models
-
-    @abstractmethod
-    def __init__(self, model_name **kwargs):
-        self.model_name = model_name
-
     @abstractmethod
     def generate(self, prompt):
         #generate text using language model
