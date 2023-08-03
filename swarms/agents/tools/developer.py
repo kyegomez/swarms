f2d8dd30e25dad5dcdec3a5e7a69e4a1e892cdd6
Kye <kye@apacmediasolutions.com>
2023-08-03 12:08:57 -0400

    helpers organization in developers tools

--- swarms/agents/tools/developer.py
+++ swarms/agents/tools/developer.py
@@ -25,6 +25,24 @@ from swarms.utils.logger import logger
 from swarms.utils.main import ANSI, Color, Style  # test
 
 
+#helpers
+PipeType = Union[Literal["stdout"], Literal["stderr"]]
+
+
+def verify(func):
+    def wrapper(*args, **kwargs):
+        try:
+            filepath = args[0].filepath
+        except AttributeError:
+            raise Exception("This tool doesn't have filepath. Please check your code.")
+        if not str(Path(filepath).resolve()).startswith(str(Path().resolve())):
+            return "You can't access file outside of playground."
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+
 class SyscallTimeoutException(Exception):
     def __init__(self, pid: int, *args) -> None:
         super().__init__(f"deadline exceeded while waiting syscall for {pid}", *args)
@@ -114,7 +132,6 @@ class SyscallTracer:
         return exitcode, reason
 
 
-PipeType = Union[Literal["stdout"], Literal["stderr"]]
 
 
 class StdoutTracer:
@@ -222,19 +239,6 @@ class Terminal(BaseToolSet):
         return output
 
 
-
-
-def verify(func):
-    def wrapper(*args, **kwargs):
-        try:
-            filepath = args[0].filepath
-        except AttributeError:
-            raise Exception("This tool doesn't have filepath. Please check your code.")
-        if not str(Path(filepath).resolve()).startswith(str(Path().resolve())):
-            return "You can't access file outside of playground."
-        return func(*args, **kwargs)
-
-    return wrapper
 
 
 
