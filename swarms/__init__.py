1df0c7c563c0e7feb663e534c79c3c0846fda6af
Kye <kye@apacmediasolutions.com>
2023-08-05 18:40:46 -0400

    base language model imports
    Former-commit-id: db2e72b788db358f23ed6a1ebcf1c7fd9ae9d345

--- swarms/__init__.py
+++ swarms/__init__.py
@@ -2,3 +2,8 @@ from swarms.swarms.swarms import HierarchicalSwarm, swarm
 from swarms.workers.worker_node import WorkerNodeInitializer, WorkerNode, worker_node
 from swarms.boss.boss_node import BossNodeInitializer, BossNode
 
+#models
+from swarms.agents.models.anthropic import Anthropic
+from swarms.agents.models.huggingface import HuggingFaceLLM
+from swarms.agents.models.palm import GooglePalm
+from swarms.agents.models.petals import Petals
