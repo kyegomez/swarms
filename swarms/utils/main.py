d1aa33a9ed988cf2a4911b2e27be4ccf7a6cc4a6
Kye <kye@apacmediasolutions.com>
2023-08-05 18:34:25 -0400

    documentation
    Former-commit-id: 615a01bef6a17932589b3cc624cbbeaa2f9d3df4

--- swarms/utils/main.py
+++ swarms/utils/main.py
@@ -400,7 +400,7 @@ class FileHandler:
 
 #############===========================>
 
-from swarms.agents.prompts.prompts import DATAFRAME_PROMPT
+from swarms.agents.models.prompts.prompts import DATAFRAME_PROMPT
 
 import pandas as pd
 class CsvToDataframe(BaseHandler):
