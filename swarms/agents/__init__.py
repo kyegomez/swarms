bd223e723d5347e58f8d781fcc69cbe080f4e3df
Kye <kye@apacmediasolutions.com>
2023-08-09 18:16:22 -0400

    no tools import
    Former-commit-id: c12d11b64b5cd2aadf3e0276e0b6901ceff2a49e

--- swarms/agents/__init__.py
+++ swarms/agents/__init__.py
@@ -9,13 +9,13 @@ from swarms.agents.models.petals import Petals
 from swarms.agents.models.openai import OpenAI
 
 ###########
-#tools
-from swarms.agents.tools.base import BaseTool, Tool, StructuredTool, ToolWrapper, BaseToolSet, ToolCreator, GlobalToolsCreator, SessionToolsCreator, ToolsFactory
-from swarms.agents.tools.autogpt import pushd, process_csv, async_load_playwright, run_async, browse_web_page, WebpageQATool, web_search, query_website_tool
-from swarms.agents.tools.exit_conversation import ExitConversation
+# #tools
+# from swarms.agents.tools.base import BaseTool, Tool, StructuredTool, ToolWrapper, BaseToolSet, ToolCreator, GlobalToolsCreator, SessionToolsCreator, ToolsFactory
+# from swarms.agents.tools.autogpt import pushd, process_csv, async_load_playwright, run_async, browse_web_page, WebpageQATool, web_search, query_website_tool
+# from swarms.agents.tools.exit_conversation import ExitConversation
 
-from swarms.agents.tools.models import MaskFormer, ImageEditing, InstructPix2Pix, Text2Image, VisualQuestionAnswering, ImageCaptioning
-from swarms.agents.tools.file_mangagement import read_tool, write_tool, list_tool
-from swarms.agents.tools.requests import RequestsGet
+# from swarms.agents.tools.models import MaskFormer, ImageEditing, InstructPix2Pix, Text2Image, VisualQuestionAnswering, ImageCaptioning
+# from swarms.agents.tools.file_mangagement import read_tool, write_tool, list_tool
+# from swarms.agents.tools.requests import RequestsGet
 
-from swarms.agents.tools.developer import Terminal, CodeEditor
+# from swarms.agents.tools.developer import Terminal, CodeEditor
