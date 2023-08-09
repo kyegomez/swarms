4a233f160c42e5f2096754c2e24a1d9f6e26eb32
Kye <kye@apacmediasolutions.com>
2023-08-09 17:53:37 -0400

    1.3.1
    Former-commit-id: e966d5f1b442642e7740fda43dfdb78b10e8adb5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'swarms',
   packages = find_packages(exclude=[]),
-  version = '1.3.0',
+  version = '1.3.1',
   license='MIT',
   description = 'Swarms - Pytorch',
   author = 'Kye Gomez',
