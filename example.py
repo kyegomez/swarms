3b31a23edeab92b083ed267c0fd16370ee4d2da9
Kye <kye@apacmediasolutions.com>
2023-08-09 10:03:49 -0400

    1.1.8

--- example.py
+++ example.py
@@ -1,24 +1,31 @@
-import os
-from swarms.swarms.swarms import HierarchicalSwarm
+# import os
+# from swarms.swarms.swarms import HierarchicalSwarm
 
-api_key = os.getenv("OPENAI_API_KEY")
+# api_key = os.getenv("OPENAI_API_KEY")
 
-# Initialize Swarms with your API key
-swarm = HierarchicalSwarm(openai_api_key=api_key)
+# # Initialize Swarms with your API key
+# swarm = HierarchicalSwarm(openai_api_key=api_key)
 
-# Define an objective
-objective = """
-Please make a web GUI for using HTTP API server. 
-The name of it is Swarms. 
-You can check the server code at ./main.py. 
-The server is served on localhost:8000. 
-Users should be able to write text input as 'query' and url array as 'files', and check the response. 
-Users input form should be delivered in JSON format. 
-I want it to have neumorphism-style. Serve it on port 4500.
+# # Define an objective
+# objective = """
+# Please make a web GUI for using HTTP API server. 
+# The name of it is Swarms. 
+# You can check the server code at ./main.py. 
+# The server is served on localhost:8000. 
+# Users should be able to write text input as 'query' and url array as 'files', and check the response. 
+# Users input form should be delivered in JSON format. 
+# I want it to have neumorphism-style. Serve it on port 4500.
 
-"""
+# """
 
-# Run Swarms
-task = swarm.run(objective)
+# # Run Swarms
+# task = swarm.run(objective)
 
-print(task)
+# print(task)
+
+
+from swarms import OpenAI
+
+chat = OpenAI()
+response = chat.generate("Hello World")
+print(response)
