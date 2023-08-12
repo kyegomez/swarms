f4948b56152d31d4eca48d2a39fb48a7c6c3db04
Kye <kye@apacmediasolutions.com>
2023-08-11 22:54:23 -0400

    example
    Former-commit-id: e7c22edca4645b5b755b00ab7109f92e00bde449

--- example.py
+++ example.py
@@ -1,27 +1,27 @@
-# import os
-# from swarms.swarms.swarms import HierarchicalSwarm
+import os
+from swarms.swarms.swarms import HierarchicalSwarm
 
-# api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
 
-# # Initialize Swarms with your API key
-# swarm = HierarchicalSwarm(openai_api_key=api_key)
+# Initialize Swarms with your API key
+swarm = HierarchicalSwarm(openai_api_key=api_key)
 
-# # Define an objective
-# objective = """
-# Please make a web GUI for using HTTP API server. 
-# The name of it is Swarms. 
-# You can check the server code at ./main.py. 
-# The server is served on localhost:8000. 
-# Users should be able to write text input as 'query' and url array as 'files', and check the response. 
-# Users input form should be delivered in JSON format. 
-# I want it to have neumorphism-style. Serve it on port 4500.
+# Define an objective
+objective = """
+Please make a web GUI for using HTTP API server. 
+The name of it is Swarms. 
+You can check the server code at ./main.py. 
+The server is served on localhost:8000. 
+Users should be able to write text input as 'query' and url array as 'files', and check the response. 
+Users input form should be delivered in JSON format. 
+I want it to have neumorphism-style. Serve it on port 4500.
 
-# """
+"""
 
-# # Run Swarms
-# task = swarm.run(objective)
+# Run Swarms
+task = swarm.run(objective)
 
-# print(task)
+print(task)
 
 
 
@@ -35,12 +35,12 @@
 
 
 
-############# v3
-import os 
-from swarms.workers.vortex_worker import VortexWorkerAgent
+# ############# v3
+# import os 
+# from swarms.workers.vortex_worker import VortexWorkerAgent
 
-api_key = os.getenv("OPENAI_API_KEY")
+# api_key = os.getenv("OPENAI_API_KEY")
 
-agent = VortexWorkerAgent(openai_api_key=api_key)
+# agent = VortexWorkerAgent(openai_api_key=api_key)
 
-agent.run("Help me find resources about renewable energy.")
+# agent.run("Help me find resources about renewable energy.")
