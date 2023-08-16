0a07418b535676f3654707e7d97ea6795b629ebc
Kye <kye@apacmediasolutions.com>
2023-08-16 16:58:14 -0400

    swarms pyproject.toml

--- swarms/__init__.py
+++ swarms/__init__.py
@@ -1,11 +1,14 @@
-
 #swarms
 
-
-
 # worker
-
+from swarms.workers.worker_node import WorkerNode
 
 #boss
+from swarms.boss.boss_node import BossNode
 
 #models
+from swarms.agents.models.anthropic import Anthropic
+from swarms.agents.models.huggingface import HuggingFaceLLM
+from swarms.agents.models.palm import GooglePalm
+from swarms.agents.models.petals import Petals
+from swarms.agents.models.openai import OpenAI
