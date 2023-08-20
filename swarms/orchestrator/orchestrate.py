c95afbec386a794a808e1e47f1d5a6282fe06765
Kye <kye@apacmediasolutions.com>
2023-08-20 00:09:42 -0400

    final cleanup

--- swarms/orchestrator/orchestrate.py
+++ swarms/orchestrator/orchestrate.py
@@ -1,49 +1,3 @@
-#input agent or multiple: => it handles multi agent communication, it handles task assignment, task execution, report back with a status, auto scaling,  number of agent nodes,
-#make it optional to have distributed communication protocols, trco, rdp, http, microsoervice, make it optional to collect data from users runs
-"""
-# Orchestrator
-* Takes in an agent class with vector store, then handles all the communication and scales up a swarm with number of agents and handles task assignment and task completion
-
-```python
-
-from swarms import OpenAI, Orchestrator, Swarm
-
-orchestrated = Orchestrate(OpenAI, nodes=40) #handles all the task assignment and allocation and agent communication using a vectorstore as a universal communication layer and also handlles the task completion logic
-
-Objective = "Make a business website for a marketing consultancy"
-
-Swarms = (Swarms(orchestrated, auto=True, Objective))
-```
-
-
-
-
-
-In terms of architecture, the swarm might look something like this:
-
-```
-                                           (Orchestrator)
-                                             /        \
-            ( Agent)---(Communication Layer)       (Communication Layer)---(Agent)
-              /                  |                                           |                 \
-(Task Assignment)      (Task Completion)                    (Task Assignment)       (Task Completion)
-```
-
-Each LLM agent communicates with the orchestrator through a dedicated communication layer. The orchestrator assigns tasks to each LLM agent, which the agents then complete and return. This setup allows for a high degree of flexibility, scalability, and robustness.
-
-In the context of swarm LLMs, one could consider an **Omni-Vector Embedding Database** for communication. This database could store and manage the high-dimensional vectors produced by each LLM agent.
-
-- Strengths: This approach would allow for similarity-based lookup and matching of LLM-generated vectors, which can be particularly useful for tasks that involve finding similar outputs or recognizing patterns.
-
-- Weaknesses: An Omni-Vector Embedding Database might add complexity to the system in terms of setup and maintenance. It might also require significant computational resources, depending on the volume of data being handled and the complexity of the vectors. The handling and transmission of high-dimensional vectors could also pose challenges in terms of network load.
-
-
-from swarms import WorkerNode, Orchestrate
-
-Orchestrate(WorkerNode, autoscale=True, nodes=int, swarm_type="flat")
-
-"""
-#in a shared state, provide 3 communication times, during task assignment, task compeltion, and feedback or inability to complete a task.
 import threading
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List
@@ -147,9 +101,3 @@ class Orchestrator(ABC):
             logging.error(f"An error occured in swarm: {e}")
             return None
 
-
-
-
-#PRE CONFIGURED AGENTS WITH domain explicit TOOLS
-#Build your own Agent
-# Learn from previous runs in session management => it's a sucessful run => omniversal memory for all swarms  
