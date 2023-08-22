bf650dae2bbbee3d027c6d6dda78c6b1a415879a
Kye <kye@apacmediasolutions.com>
2023-08-21 20:11:10 -0400

    bugs cleanup
    Former-commit-id: add33d7411fe08b4bdf0d09f9ef244a74c351a72

--- playground/autoscaler.py
+++ playground/autoscaler.py
@@ -1,4 +1,4 @@
-from swarms import AutoBot, AutoScaler
+from swarms import AutoScaler
 
 auto_scaler = AutoScaler()
 auto_scaler.start()
@@ -6,4 +6,3 @@ auto_scaler.start()
 for i in range(100):
     auto_scaler.add_task(f"Task {i}")
 
-    
