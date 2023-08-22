ba2689204a4682f756c0e0eb5bb76085c9111de0
Kye <kye@apacmediasolutions.com>
2023-08-22 07:50:20 -0400

    Worker autonomous agent example
    Former-commit-id: 972b55e760876f50892eac97ae13c4ecf5289eaa

--- example.py
+++ example.py
@@ -67,14 +67,14 @@
 
 
 ##########
-from swarms import AutoBot
+from swarms import Worker
 
 
-worker = AutoBot(
+node = Worker(
     openai_api_key="",
     ai_name="Optimus Prime",
 )
 
 task = "What were the winning boston marathon times for the past 5 years (ending in 2022)? Generate a table of the year, name, country of origin, and times."
-response = worker.run(task)
+response = node.run(task)
 print(response)
