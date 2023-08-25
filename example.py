5a75354815376c4b20258b37e03b1a68fca5897d
Kye <kye@apacmediasolutions.com>
2023-08-25 14:27:22 -0400

    clean up external tools
    Former-commit-id: 1198976041c1757b3116c8ca5456620c87693470

--- example.py
+++ example.py
@@ -4,6 +4,7 @@ from swarms import Worker
 node = Worker(
     openai_api_key="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",
     ai_name="Optimus Prime",
+    
 )
 
 task = "What were the winning boston marathon times for the past 5 years (ending in 2022)? Generate a table of the year, name, country of origin, and times."
