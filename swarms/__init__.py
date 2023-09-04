612982850904202ff13cdbc9e17c458ebff54248
Kye <kye@apacmediasolutions.com>
2023-09-03 20:42:54 -0400

    openai class and error cleanups
    Former-commit-id: c3c9d18cfde4fe6953c6133c23d12c89285b6ed6

--- swarms/__init__.py
+++ swarms/__init__.py
@@ -13,6 +13,6 @@ from swarms.models.anthropic import Anthropic
 from swarms.models.huggingface import HuggingFaceLLM
 # from swarms.models.palm import GooglePalm
 from swarms.models.petals import Petals
-from swarms.models.openai import OpenAI
+from swarms.models.openai import OpenAIChat
 
 
