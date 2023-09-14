ee5b86f560b3c7ef4aa2e23335bfb4e153fd6119
Kye <kye@apacmediasolutions.com>
2023-09-14 18:22:46 -0400

    orchestrator class

--- swarms/swarms/orchestrate.py
+++ swarms/swarms/orchestrate.py
@@ -9,8 +9,62 @@ import chromadb
 from chromadb.utils import embedding_functions
 
 
-## =========> 
 class Orchestrator(ABC):
+    """
+
+    The Orchestrator takes in an agent, worker, or boss as input 
+    then handles all the logic for
+    - task creation,
+    - task assignment,
+    - and task compeletion.
+
+    And, the communication for millions of agents to communicate with eachother through
+    a vector database that each agent has access to communicate with.
+
+    Each LLM agent communicates with the orchestrator through a dedicated 
+    communication layer. The orchestrator assigns tasks to each LLM agent,
+    which the agents then complete and return. 
+
+    This setup allows for a high degree of flexibility, scalability, and robustness.
+
+    In the context of swarm LLMs, one could consider an **Omni-Vector Embedding Database
+    for communication. This database could store and manage 
+    the high-dimensional vectors produced by each LLM agent.
+
+    Strengths: This approach would allow for similarity-based lookup and matching of 
+    LLM-generated vectors, which can be particularly useful for tasks that involve finding similar outputs or recognizing patterns.
+
+    Weaknesses: An Omni-Vector Embedding Database might add complexity to the system in terms of setup and maintenance. 
+    It might also require significant computational resources, 
+    depending on the volume of data being handled and the complexity of the vectors. 
+    The handling and transmission of high-dimensional vectors could also pose challenges 
+    in terms of network load.
+
+    # Orchestrator
+    * Takes in an agent class with vector store, 
+    then handles all the communication and scales 
+    up a swarm with number of agents and handles task assignment and task completion
+
+    from swarms import OpenAI, Orchestrator, Swarm
+
+    orchestrated = Orchestrate(OpenAI, nodes=40) #handles all the task assignment and allocation and agent communication using a vectorstore as a universal communication layer and also handlles the task completion logic
+
+    Objective = "Make a business website for a marketing consultancy"
+
+    Swarms = (Swarms(orchestrated, auto=True, Objective))
+    ```
+
+    In terms of architecture, the swarm might look something like this:
+
+    ```
+                                            (Orchestrator)
+                                                /        \
+            Tools + Vector DB -- (LLM Agent)---(Communication Layer)       (Communication Layer)---(LLM Agent)-- Tools + Vector DB 
+                /                  |                                           |                 \
+    (Task Assignment)      (Task Completion)                    (Task Assignment)       (Task Completion)
+
+
+    """
     def __init__(
         self, 
         agent, 
@@ -80,7 +134,7 @@ class Orchestrator(ABC):
 
         embedding = openai(input)
         # print(embedding)
-        
+
         embedding_metadata = {input: embedding}
         print(embedding_metadata)
 
