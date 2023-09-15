349e683e4158857cedc55a0f9cafb91c8aef6138
Kye <kye@apacmediasolutions.com>
2023-09-15 14:03:51 -0400

    `Workflow`
    Former-commit-id: 8058bb1026cb228865242cc5fa1c5312c8437dd6

---
+++ swarms/artifacts/error_artifact.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+from attr import define, field
+from swarms.artifacts.base import BaseArtifact
+
+
+@define(frozen=True)
+class ErrorArtifact(BaseArtifact):
+    value: str = field(converter=str)
+
+    def __add__(self, other: ErrorArtifact) -> ErrorArtifact:
+        return ErrorArtifact(self.value + other.value)
+    
+    def to_text(self) -> str:
+        return self.value
+    
+    def to_dict(self) -> dict:
+        from griptape.schemas import ErrorArtifactSchema
+
+        return dict(ErrorArtifactSchema().dump(self))
+    
