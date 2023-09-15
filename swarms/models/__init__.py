cd34ba68447a14e3bc54808bf66f5b7a4da72b8b
Kye <kye@apacmediasolutions.com>
2023-09-15 19:08:41 -0400

    clean up
    Former-commit-id: 0eb84344f596ba44ae7b3e9a5ef009d9588b7023

--- swarms/models/__init__.py
+++ swarms/models/__init__.py
@@ -1,5 +1,5 @@
 from swarms.models.anthropic import Anthropic
-from swarms.models.huggingface import HuggingFaceLLM
+from swarms.models.huggingface import HFLLM
 # from swarms.models.palm import GooglePalm
 from swarms.models.petals import Petals
 #from swarms.models.openai import OpenAIChat
