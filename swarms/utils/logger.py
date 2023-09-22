31be4be43967e40577f35cd3cdd0546ba96e6630
Kye <kye@apacmediasolutions.com>
2023-09-22 00:36:16 -0400

    clean up
    Former-commit-id: 29b82b595b1eefe667f724bd193a60cd07c94f8a

--- swarms/utils/logger.py
+++ swarms/utils/logger.py
@@ -2,8 +2,11 @@ import logging
 
 logger = logging.getLogger()
 formatter = logging.Formatter("%(message)s")
+
 ch = logging.StreamHandler()
+
 ch.setFormatter(formatter)
+
 logger.addHandler(ch)
 
 logger.setLevel(logging.DEBUG)
