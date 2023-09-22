3de0095af37258daa77bb7d08865eb350c7d5c81
Kye <kye@apacmediasolutions.com>
2023-09-22 18:06:00 -0400

    clean up
    Former-commit-id: f2f5f79dc7c16a9e53a15f0c5e323b5167686a4d

--- example.py
+++ example.py
@@ -9,15 +9,3 @@ node = Worker(
 task = "What were the winning boston marathon times for the past 5 years (ending in 2022)? Generate a table of the year, name, country of origin, and times."
 response = node.run(task)
 print(response)
-
-
-# from swarms import Workflow
-# from swarms.tools.autogpt import ChatOpenAI
-
-# workflow = Workflow(ChatOpenAI)
-
-# workflow.add("What's the weather in miami")
-# workflow.add("Provide details for {{ parent_output }}")
-# workflow.add("Summarize the above information: {{ parent_output}}")
-
-# workflow.run()
