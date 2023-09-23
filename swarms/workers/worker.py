4d97824b67ab04287089ddfa553be0b63113f8a4
Kye <kye@apacmediasolutions.com>
2023-09-23 17:03:04 -0400

    clean up

--- swarms/workers/worker.py
+++ swarms/workers/worker.py
@@ -25,14 +25,16 @@ class Worker:
     @log_decorator
     @error_decorator
     @timing_decorator
-    def __init__(self, 
-                 model_name="gpt-4", 
-                 openai_api_key=None,
-                 ai_name="Autobot Swarm Worker",
-                 ai_role="Worker in a swarm",
-                 external_tools = None,
-                human_in_the_loop=False,
-                 temperature=0.5):
+    def __init__(
+        self, 
+        model_name="gpt-4", 
+        openai_api_key=None,
+        ai_name="Autobot Swarm Worker",
+        ai_role="Worker in a swarm",
+        external_tools = None,
+        human_in_the_loop=False,
+        temperature=0.5
+    ):
         self.openai_api_key = openai_api_key
         self.temperature = temperature
         self.human_in_the_loop = human_in_the_loop
