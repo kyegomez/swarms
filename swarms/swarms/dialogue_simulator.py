65e34b57509a267b1a66c4d4b86849b9f5602a3b
Kye <kye@apacmediasolutions.com>
2023-09-23 23:11:17 -0400

    dialogue simulator
    Former-commit-id: de9781df02e1960aa39e803b3d0dd3fc2aa0389b

---
+++ swarms/swarms/dialogue_simulator.py
@@ -0,0 +1,48 @@
+from typing import List, Callable
+
+
+class DialogueSimulator:
+    def __init__(
+        self,
+        agents,
+        selection_func
+    ):
+        self.agents = agents
+        self._step = 0
+        self.select_next_speaker = selection_func
+    
+    def reset(self):
+        for agent in self.agents:
+            agent.reset()
+        
+    def start(self, name: str, message: str):
+        #init conv with a message from name
+        for agent in self.agents:
+            agent.receive(name, message)
+        
+        #increment time
+        self._step += 1
+    
+    def step(self) -> tuple[str, str]:
+        #choose next speaker
+        speaker_idx = self.select_next_speaker(
+            self._step,
+            self.agent
+        )
+        speaker = self.agents[speaker_idx]
+
+        #2. next speaker ends message
+        message = speaker.send()
+
+        #everyone receives messages
+        for receiver in self.agents:
+            receiver.receive(speaker.name, message)
+        
+        #increment time
+        self._step += 1
+
+        return speaker.name, message
+    
+    def select_next_speaker(step: int, agents) -> int:
+        idx = (step) % len(agents)
+        return idx
