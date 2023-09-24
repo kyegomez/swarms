c9fb8aeb0a7c1de87e49f6a84d87d2f33ac53d18
Kye <kye@apacmediasolutions.com>
2023-09-24 19:58:57 -0400

    clean up for multi agent collab
    Former-commit-id: 85a3311dbaea6960b7c8680cc7d7607e25208f39

--- swarms/__init__.py
+++ swarms/__init__.py
@@ -1,13 +1,13 @@
 #swarms
-from swarms.logo import logo
-print(logo)
+from swarms.logo import logo2
+print(logo2)
 #from swarms.orchestrator.autoscaler import AutoScaler
 
 # worker
 # from swarms.workers.worker_node import WorkerNode
 
 #boss
-from swarms.boss.boss_node import Boss
+# from swarms.boss.boss_node import Boss
 
 #models
 from swarms.models.anthropic import Anthropic
@@ -23,5 +23,6 @@ from swarms.workers.worker import Worker
 from swarms.structs.workflow import Workflow
 
 # swarms
-from swarms.swarms.orchestrate import Orchestrator
+from swarms.swarms.dialogue_simulator import DialogueSimulator
 from swarms.swarms.autoscaler import AutoScaler
+from swarms.swarms.orchestrate import Orchestrator
--- swarms/models/__init__.py
+++ swarms/models/__init__.py
@@ -2,3 +2,6 @@ from swarms.models.anthropic import Anthropic
 # from swarms.models.palm import GooglePalm
 from swarms.models.petals import Petals
 #from swarms.models.openai import OpenAIChat
+
+#prompts
+from swarms.models.prompts.debate import *
