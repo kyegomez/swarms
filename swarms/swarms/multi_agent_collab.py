78cc3ccf6fb15a84d48204bfc1d11bd9bc1ca75e
Kye <kye@apacmediasolutions.com>
2023-09-24 20:13:01 -0400

    examples
    Former-commit-id: c1b35111b5fb15de98321887285f335cd6e1c3f2

--- swarms/swarms/multi_agent_collab.py
+++ swarms/swarms/multi_agent_collab.py
@@ -11,6 +11,19 @@ bid_parser = BidOutputParser(
     regex=r"<(\d+)>", output_keys=["bid"], default_output_key="bid"
 )
 
+def select_next_speaker(
+        step: int,
+        agents,
+        director
+) -> int:
+    #if the step if even => director
+    #=> director selects next speaker
+    if step % 2 == 1:
+        idx = 0
+    else:
+        idx = director.select_next_speaker() + 1
+    return idx
+
 
 #main
 class MultiAgentCollaboration:
