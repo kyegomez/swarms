ec22711d0980bc80081632ae56e3ca7560356fca
Kye <kye@apacmediasolutions.com>
2023-09-25 14:12:20 -0400

    clean up agents

--- swarms/workers/multi_modal_worker.py
+++ swarms/workers/multi_modal_worker.py
@@ -1,11 +1,7 @@
 # from swarms.workers.multi_modal_workers.multi_modal_agent import MultiModalVisualAgent
 from swarms.workers.multi_modal_workers.multi_modal_agent import MultiModalVisualAgent
-from langchain.tools import BaseTool
 
-class MultiModalVisualAgentTool(BaseTool):
-    name = "multi_visual_agent"
-    description = "Multi-Modal Visual agent tool"
-
+class MultiModalVisualAgentTool:
     def __init__(self, agent: MultiModalVisualAgent):
         self.agent = agent
     
@@ -13,6 +9,3 @@ class MultiModalVisualAgentTool(BaseTool):
         #run the multi-modal visual agent with the give task
         return self.agent.run_text(text)
 
-
-multimodal_agent = MultiModalVisualAgent()
-multimodal_agent_tool = MultiModalVisualAgentTool(multimodal_agent)
