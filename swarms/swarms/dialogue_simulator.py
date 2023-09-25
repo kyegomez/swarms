f1a50b403fe02b93f2c7dd8c9d8acee69562be5b
Kye <kye@apacmediasolutions.com>
2023-09-25 13:23:24 -0400

    clean up
    Former-commit-id: b050ad1b584695ccecb9c24cdff159231abd632d

--- swarms/swarms/dialogue_simulator.py
+++ swarms/swarms/dialogue_simulator.py
@@ -6,15 +6,15 @@ class DialogueSimulator:
     def __init__(
         self,
         agents: List[Worker],
-        selection_func
+        selection_func: Callable[[int, List[Worker]], int],
     ):
         self.agents = agents
         self._step = 0
         self.select_next_speaker = selection_func
     
-    def reset(self):
-        for agent in self.agents:
-            agent.reset()
+    # def reset(self):
+    #     for agent in self.agents:
+    #         agent.reset()
         
     def start(self, name: str, message: str):
         #init conv with a message from name
