a1756e6843001b87640ad279a9a4cdc36b870c0e
Kye <kye@apacmediasolutions.com>
2023-09-25 13:18:12 -0400

    clean up

--- dialogue_simulator.py
+++ dialogue_simulator.py
@@ -1,6 +1,6 @@
 from swarms import DialogueSimulator, Worker
 
-worker1 = Worker(ai_name="Optimus Prime", openai_api_key="")
+worker1 = Worker(ai_name="Plinus", openai_api_key="")
 worker2 = Worker(ai_name="Optimus Prime", openai_api_key="")
 
 collab = DialogueSimulator([worker1, worker2], DialogueSimulator.select_next_speaker)
