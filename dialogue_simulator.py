557d952eef690c093155dd60d0a1ce439d340991
Kye <kye@apacmediasolutions.com>
2023-09-25 13:32:27 -0400

    clean up
    Former-commit-id: 4c521437a0b346f19985a39f41c9fda0259f8e7c

--- dialogue_simulator.py
+++ dialogue_simulator.py
@@ -4,7 +4,7 @@ worker1 = Worker(ai_name="Plinus", openai_api_key="")
 worker2 = Worker(ai_name="Optimus Prime", openai_api_key="")
 
 collab = DialogueSimulator([worker1, worker2], DialogueSimulator.select_next_speaker)
-collab.step()
+collab.reset()
 
 # collab.start("My name is Plinus and I am a worker", "How are you?")
 
@@ -12,7 +12,7 @@ max_iters = 6
 n = 0
 
 while n < max_iters:
-    collab.start("My name is Plinus and I am a worker", "How are you?")
+    name, message = simulator.step()
     print(f"({name}): {message}")
     print("\n")
     n += 1
