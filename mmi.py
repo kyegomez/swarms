555e3820d9e799a46c8a2c9f771cda0f76391d00
Kye <kye@apacmediasolutions.com>
2023-09-25 13:05:12 -0400

    clean up
    Former-commit-id: 9c4236f5164e7f9f863fa5a9a4a2089a38b2dcaf

--- mmi.py
+++ mmi.py
@@ -1,5 +1,7 @@
 from swarms import DialogueSimulator, Worker
 
-collab = DialogueSimulator(Worker, DialogueSimulator.select_next_speaker)
-collab.start("My name is Plinus and I am a worker", "How are you?")
+worker = Worker(ai_name="Optimus Prime", openai_api_key="")
 
+collab = DialogueSimulator(worker, DialogueSimulator.select_next_speaker)
+
+collab.start("My name is Plinus and I am a worker", "How are you?")
