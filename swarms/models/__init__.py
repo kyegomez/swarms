c362cc7be4c4088849c82bbda7afd8b4a10e4186
Kye <kye@apacmediasolutions.com>
2023-09-26 18:40:33 -0400

    inits
    Former-commit-id: 4e068f8a751af559a168f36cc95cfdf867c682a8

--- swarms/models/__init__.py
+++ swarms/models/__init__.py
@@ -1,7 +1,6 @@
 from swarms.models.anthropic import Anthropic
-# from swarms.models.palm import GooglePalm
+from swarms.models.palm import GooglePalm
 from swarms.models.petals import Petals
-#from swarms.models.openai import OpenAIChat
-
+from swarms.models.openai import OpenAIChat
 #prompts
 from swarms.models.prompts.debate import *
--- swarms/swarms/__init__.py
+++ swarms/swarms/__init__.py
@@ -1,10 +1,8 @@
-
 # swarms
 from swarms.swarms.dialogue_simulator import DialogueSimulator
 from swarms.swarms.autoscaler import AutoScaler
 from swarms.swarms.orchestrate import Orchestrator
 from swarms.swarms.god_mode import GodMode
 from swarms.swarms.simple_swarm import SimpleSwarm
-from swarms.swarms.multi_agent_debate import MultiAgentDebate
+from swarms.swarms.multi_agent_debate import MultiAgentDebate, select_speaker
 from swarms.swarms.groupchat import GroupChat
-
