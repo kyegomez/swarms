1709eee1d0de83a9d8fae170d5d26e0d70e77dde
Kye <kye@apacmediasolutions.com>
2023-09-25 23:19:37 -0400

    clean up
    Former-commit-id: cf63a539046841eaf94fcc05bab04e32ce9f2a19

--- swarms/agents/multi_modal_agent.py
+++ swarms/agents/multi_modal_agent.py
@@ -1,3 +1,5 @@
+from agent_protocol import Agent, Step, Task
+
 from swarms.workers.multi_modal_workers.multi_modal_agent import MultiModalVisualAgent
 
 class MultiModalVisualAgent:
@@ -6,8 +8,27 @@ class MultiModalVisualAgent:
             agent: MultiModalVisualAgent
         ):
         self.agent = agent
+        self.plan = plan
     
-    def _run(self, text: str) -> str:
+    async def run(self, text: str) -> str:
         #run the multi-modal visual agent with the give task
         return self.agent.run_text(text)
+    
+    async def __call__(self, text: str) -> str:
+        return self.agent.run(text)
+    
+    async def plan(self, step: Step) -> Step:
+        task = Agent
+        pass
 
+    async def task_handler(self, task: Task):
+        await self.agent.run()
+    
+    async def step_handler(self, step: Step):
+        if step.name == "plan":
+            await self.plan(step)
+        else:
+            await self.agent.run(step)
+
+        return step
+    
