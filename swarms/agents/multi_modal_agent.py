c93de1f84eb1db44bb7be11145a2f47584d6a74e
Kye <kye@apacmediasolutions.com>
2023-10-03 13:58:37 -0400

    message
    Former-commit-id: fd680fbe8ade61ad11ca6126a660ab0217aa9212

--- swarms/agents/multi_modal_agent.py
+++ swarms/agents/multi_modal_agent.py
@@ -1,33 +1,69 @@
-from agent_protocol import Agent, Step, Task
+from swarms.agents.muti_modal_workers.multi_modal_agent import MultiModalVisualAgent
 
-from swarms.agents.multi_modal_workers.multi_modal_agent import MultiModalVisualAgent
+class MultiModalAgent:
+    """
+    A user-friendly abstraction over the MultiModalVisualAgent that provides a simple interface 
+    to process both text and images.
+    
+    Initializes the MultiModalAgent.
 
-class MultiModalVisualAgent:
+        Parameters:
+            load_dict (dict, optional): Dictionary of class names and devices to load. Defaults to a basic configuration.
+            temperature (float, optional): Temperature for the OpenAI model. Defaults to 0.
+            default_language (str, optional): Default language for the agent. Defaults to "English".
+
+    Usage
+    
+    """
     def __init__(
-            self, 
-            agent: MultiModalVisualAgent
-        ):
-        self.agent = agent
-    
-    async def run(self, text: str) -> str:
-        #run the multi-modal visual agent with the give task
-        return self.agent.run_text(text)
-    
-    async def __call__(self, text: str) -> str:
-        return self.agent.run(text)
-    
-    async def plan(self, step: Step) -> Step:
-        task = Agent
-        pass
+        self,
+        load_dict,
+        temperature,
+        language: str = "english"
+    ):
+        self.load_dict = load_dict
+        self.temperature = temperature
+        self.langigage = language
 
-    async def task_handler(self, task: Task):
-        await self.agent.run()
-    
-    async def step_handler(self, step: Step):
-        if step.name == "plan":
-            await self.plan(step)
-        else:
-            await self.agent.run(step)
+        if load_dict is None:
+            load_dict = {
+                "ImageCaptioning": "default_device"
+            }
 
-        return step
+        self.agent = MultiModalVisualAgent(
+            load_dict,
+            temperature
+        )
+        self.language = language
+    
+    def run_text(self, text, language=None):
+        """Run text through the model"""
+
+        if language is None:
+            language = self.language
+
+        try:
+            self.agent.init_agent(language)
+            return self.agent.run_text(text)
+        except Exception as e:
+            return f"Error processing text: {str(e)}"
+    
+    def run_img(self, image_path: str, language=None):
+        """If language is None"""
+        if language is None:
+            language = self.default_language
+        
+        try:
+            return self.agent.run_image(
+                image_path,
+                language
+            )
+        except Exception as error:
+            return f"Error processing image: {str(error)}"
+    
+    def clear(self):
+        try:
+            self.agent.clear_memory()
+        except Exception as e:
+            return f"Error cleaning memory: {str(e)}"
     
