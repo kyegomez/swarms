48f6a79c333eae9836cfa2dbba8228208fac61b1
Kye <kye@apacmediasolutions.com>
2023-10-03 16:41:05 -0400

    no palm
    Former-commit-id: 2780a8edc00f4e4c18fb301d2b0efe6684dc81c6

--- swarms/models/__init__.py
+++ swarms/models/__init__.py
@@ -1,5 +1,5 @@
 from swarms.models.anthropic import Anthropic
-from swarms.models.palm import GooglePalm
+# from swarms.models.palm import GooglePalm
 from swarms.models.petals import Petals
 from swarms.models.openai import OpenAIChat
 #prompts
