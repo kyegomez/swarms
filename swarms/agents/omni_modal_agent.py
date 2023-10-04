d08f61dbf1bd7e2b614fc22246038ecff48d0f56
Kye <kye@apacmediasolutions.com>
2023-10-04 15:42:42 -0400

    omnimodal agent
    Former-commit-id: e1e7bd8988999be3f4d5ad551776b8c3a83adf82

--- swarms/agents/omni_modal_agent.py
+++ swarms/agents/omni_modal_agent.py
@@ -13,34 +13,6 @@ from langchain_experimental.autonomous_agents.hugginggpt.task_planner import (
 )
 from transformers import load_tool
 
-# from swarms.agents.multi_modal_workers.omni_agent.omni_chat import chat_huggingface
-# class OmniModalAgent:
-#     def __init__(
-#         self, 
-#         api_key, 
-#         api_endpoint, 
-#         api_type
-#     ):
-#         self.api_key = api_key
-#         self.api_endpoint = api_endpoint
-#         self.api_type = api_type
-
-#     def chat(
-#         self, 
-#         data
-#     ):
-#         """Chat with omni-modality model that uses huggingface to query for a specific model at run time. Translate text to speech, create images and more"""
-#         messages = data.get("messages")
-#         api_key = data.get("api_key", self.api_key)
-#         api_endpoint = data.get("api_endpoint", self.api_endpoint)
-#         api_type = data.get("api_type", self.api_type)
-
-#         if not(api_key and api_type and api_endpoint):
-# #             raise ValueError("Please provide api_key, api_type, and api_endpoint")
-        
-# #         response = chat_huggingface(messages, api_key, api_type, api_endpoint)
-# #         return response
-
 class Step:
     def __init__(
         self,
