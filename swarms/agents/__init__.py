1a46dabc7877a28450411cb405b2af7a883f1884
Kye <kye@apacmediasolutions.com>
2023-10-06 14:35:12 -0400

    clean up
    Former-commit-id: e0f43399d5a4335cb07929cb57938aecae2f22b8

--- swarms/agents/__init__.py
+++ swarms/agents/__init__.py
@@ -12,4 +12,4 @@ from swarms.agents.omni_modal_agent import OmniModalAgent
 #utils
 from swarms.agents.message import Message
 from swarms.agents.stream_response import stream
-# from swarms.agents.base import AbstractAgent
+from swarms.agents.base import Agent
