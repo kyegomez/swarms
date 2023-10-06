49eea7e4f5bd3bab6a706e7c2908c65822444a46
Kye <kye@apacmediasolutions.com>
2023-10-06 12:34:38 -0400

    clean up
    Former-commit-id: 793a8cf2f803e2f68adbb11930310473545c0a0c

--- swarms/agents/aot.py
+++ swarms/agents/aot.py
@@ -2,8 +2,6 @@ import logging
 import os
 import time
 
-import llm
-
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
