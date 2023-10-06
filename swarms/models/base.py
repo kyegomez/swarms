bb971bcb252fcc7f93bed7735047dad79db97962
Kye <kye@apacmediasolutions.com>
2023-10-06 02:06:54 -0400

    mistral ai model
    Former-commit-id: 75739e6d00e8338d2056715083cd6e3db4197998

--- swarms/models/base.py
+++ swarms/models/base.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 
 class AbstractModel(ABC):
     #abstract base class for language models
+    def __init__():
+        pass
+    
     @abstractmethod
     def run(self, prompt):
         #generate text using language model
