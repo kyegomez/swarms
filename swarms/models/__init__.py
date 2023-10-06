1ab43903730a8b3e4e0abb5ac20fe9d3a5239fcb
Kye <kye@apacmediasolutions.com>
2023-10-06 10:24:25 -0400

    clean up

--- swarms/models/__init__.py
+++ swarms/models/__init__.py
@@ -2,6 +2,6 @@
 from swarms.models.anthropic import Anthropic
 # from swarms.models.palm import GooglePalm
 from swarms.models.petals import Petals
-from swarms.models.chat_openai import OpenAIChat
+# from swarms.models.chat_openai import OpenAIChat
 from swarms.models.prompts.debate import *
 from swarms.models.mistral import Mistral
