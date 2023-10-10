f53e71c73183f0deda4a1717d321c4618e882220
Kye <kye@apacmediasolutions.com>
2023-10-10 17:47:25 -0400

    clean up
    Former-commit-id: 080ade7cc48d93b6efb655a867b9fc28542905d5

--- swarms/workers/worker.py
+++ swarms/workers/worker.py
@@ -23,7 +23,8 @@ from swarms.utils.decorators import error_decorator, log_decorator, timing_decor
 ROOT_DIR = "./data/"
 
 # main
-openai_api_key = os.environ["OPENAI_API_KEY"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
 
 class Worker:
     """
@@ -72,7 +73,7 @@ class Worker:
         self.temperature = temperature
         self.human_in_the_loop = human_in_the_loop
         self.llm = llm
-        self.openai_api_key = openai_api_key or os.environ["OPENAI_API_KEY"]
+        self.openai_api_key = openai_api_key or OPENAI_API_KEY
         self.ai_name = ai_name
         self.ai_role = ai_role
         self.setup_tools(external_tools)
