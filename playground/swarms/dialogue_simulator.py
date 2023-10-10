454ee70d2266a840a0fe2ef934d69609f33a0475
Kye <kye@apacmediasolutions.com>
2023-10-10 16:57:55 -0400

    examples fix

--- playground/swarms/dialogue_simulator.py
+++ playground/swarms/dialogue_simulator.py
@@ -1,7 +1,33 @@
-from swarms import DialogueSimulator, Worker
+from swarms.swarms import DialogueSimulator
+from swarms.workers.worker import Worker
+from langchain.llms import OpenAIChat
 
-worker1 = Worker(ai_name="Plinus", openai_api_key="")
-worker2 = Worker(ai_name="Optimus Prime", openai_api_key="")
+llm = OpenAIChat(model_name="gpt-4", openai_api_key="api-key", temperature=0.5)
+
+worker1 = Worker(
+    llm=llm,
+    ai_name="Bumble Bee",
+    ai_role="Worker in a swarm",
+    external_tools=None,
+    human_in_the_loop=False,
+    temperature=0.5,
+)
+worker2 = Worker(
+    llm=llm,
+    ai_name="Optimus Prime",
+    ai_role="Worker in a swarm",
+    external_tools=None,
+    human_in_the_loop=False,
+    temperature=0.5,
+)
+worker3 = Worker(
+    llm=llm,
+    ai_name="Megatron",
+    ai_role="Worker in a swarm",
+    external_tools=None,
+    human_in_the_loop=False,
+    temperature=0.5,
+)
 
 collab = DialogueSimulator(
     [worker1, worker2],
