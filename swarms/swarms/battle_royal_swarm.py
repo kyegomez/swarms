8ad06e9dd161bfcde60f1dd2a67e868049576af4
Kye <kye@apacmediasolutions.com>
2023-10-12 02:15:53 -0400

    battle royal

---
+++ swarms/swarms/battle_royal_swarm.py
@@ -0,0 +1,17 @@
+"""
+
+Battle royal swarm where agents compete to be the first to answer a question. or the best answer.
+Look to fornight game
+
+teams of 1, 3 or 4 that equates to 100 total agents
+
+
+Communication is proximal and based on proximity
+Clashes with adversial agents not in team.
+
+Teams of 3 agents would fight each other and then move on while other agents are clashing with eachother as well.
+
+Agents can be in multiple teams
+Agents can be in multiple teams and be adversial to each other
+Agents can be in multiple teams and be adversial to each other and be in multiple teams
+"""
