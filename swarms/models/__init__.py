4c2ec18731b3b612b2d89b71e6f6df8e5e86d337
Kye <kye@apacmediasolutions.com>
2023-10-13 18:59:17 -0400

    init
    Former-commit-id: 5e4a024b81f323ea15655c8e7defc2ef7881afc8

--- swarms/models/__init__.py
+++ swarms/models/__init__.py
@@ -1,4 +1,5 @@
 from swarms.models.anthropic import Anthropic
 from swarms.models.petals import Petals
 from swarms.models.mistral import Mistral
-from swarms.models.openai_llm import OpenAIModel
+# from swarms.models.openai_llm import OpenAIModel
+from swarms.models.openai_models import OpenAI, AzureOpenAI, OpenAIChat
