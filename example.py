d27bb875bc22a377ed96b099a198b129603400c2
Kye <kye@apacmediasolutions.com>
2023-10-14 13:13:27 -0400

    clean up
    Former-commit-id: f5c02ff38adfa58b403738c8a8c84cdd6465d8ac

--- example.py
+++ example.py
@@ -2,14 +2,14 @@ from swarms.models import OpenAIChat
 from swarms import Worker
 
 llm = OpenAIChat(
-    openai_api_key="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",
+    openai_api_key="",
     temperature=0.5,
 )
 
 node = Worker(
     llm=llm,
     ai_name="Optimus Prime",
-    openai_api_key="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",
+    openai_api_key="",
     ai_role="Worker in a swarm",
     external_tools=None,
     human_in_the_loop=False,
