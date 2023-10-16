0f8a9f79e7806bb1dda79bdd262b97b46503f284
Kye <kye@apacmediasolutions.com>
2023-10-16 00:28:07 -0400

    idea 2 image
    Former-commit-id: e236416bf9bb8f6ab175f87fd59110b0704cd51d

--- swarms/agents/__init__.py
+++ swarms/agents/__init__.py
@@ -1,9 +1,4 @@
 """Agent Infrastructure, models, memory, utils, tools"""
-
-# agents
-# from swarms.agents.profitpilot import ProfitPilot
-# from swarms.agents.aot import AoTAgent
-# from swarms.agents.multi_modal_visual_agent import MultiModalAgent
 from swarms.agents.omni_modal_agent import OmniModalAgent
 from swarms.agents.hf_agents import HFAgent
 
@@ -13,3 +8,5 @@ from swarms.agents.message import Message
 from swarms.agents.stream_response import stream
 from swarms.agents.base import AbstractAgent
 from swarms.agents.registry import Registry
+from swarms.agents.idea_to_image_agent import Idea2Image
+
