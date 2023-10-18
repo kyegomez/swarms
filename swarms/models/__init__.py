ee60d8d5910d6325fd20b13e2dd2e79c9efa99d2
Kye <kye@apacmediasolutions.com>
2023-10-18 16:15:47 -0400

    Fuyu
    Former-commit-id: df0ccdcbc019b7b3fbfc50fed57b65b4224a008c

--- swarms/models/__init__.py
+++ swarms/models/__init__.py
@@ -2,3 +2,4 @@ from swarms.models.anthropic import Anthropic
 from swarms.models.petals import Petals
 from swarms.models.mistral import Mistral
 from swarms.models.openai_models import OpenAI, AzureOpenAI, OpenAIChat
+from swarms.models.fuyu import Fuyu
