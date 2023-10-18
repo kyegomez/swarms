96ecbe70a144ba106abc0ad1d21dadd442b4ce81
Kye <kye@apacmediasolutions.com>
2023-10-17 20:42:43 -0400

    clean up

---
+++ swarms/agents/refiner_agent.py
@@ -0,0 +1,15 @@
+
+
+class PromptRefiner:
+    def __init__(
+        self,
+        system_prompt: str,
+        llm
+    ):
+        super().__init__()
+        self.system_prompt = system_prompt
+        self.llm = llm
+    
+    def run(self, task: str):
+        refine = self.llm(f"System Prompt: {self.system_prompt} Current task: {task}")
+        return refine
