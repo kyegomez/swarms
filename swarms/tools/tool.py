9d2f668295dd1091730219ac651b3956b9ceb6c8
Zack <zack@zackbradshaw.com>
2023-10-19 23:46:06 -0500

    fix: tool
    Former-commit-id: 21c88ef40dfb81443d8663ac181bee63bb974c79

--- swarms/tools/tool.py
+++ swarms/tools/tool.py
@@ -849,3 +849,11 @@ def tool(
         return _partial
     else:
         raise ValueError("Too many arguments for tool decorator")
+
+class EdgeGPTTool(BaseTool):
+    def __init__(self, model, name="EdgeGPTTool", description="Tool that uses EdgeGPTModel to generate responses"):
+        super().__init__(name=name, description=description)
+        self.model = model
+
+    def _run(self, prompt):
+        return self.model.__call__(prompt)
