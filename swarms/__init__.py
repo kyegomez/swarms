434be59483b9fab12dd79689c5036b800d9eefa1
Kye <kye@apacmediasolutions.com>
2023-10-20 01:17:19 -0400

    edgegpt clean up
    Former-commit-id: 97e366ff8f9dd79b999fd2c532e727be9e5b02f4

--- swarms/__init__.py
+++ swarms/__init__.py
@@ -1,12 +1,3 @@
-from swarms import workers
-from swarms.workers.worker import Worker
-from swarms.chunkers import chunkers
-from swarms import models
-from swarms import structs
-from swarms import swarms
-from swarms.swarms.orchestrate import Orchestrator
-from swarms import agents
-from swarms.logo import logo
 import os
 import warnings
 
@@ -17,4 +8,15 @@ warnings.filterwarnings("ignore", category=UserWarning)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 
+
+
+from swarms import workers
+from swarms.workers.worker import Worker
+from swarms.chunkers import chunkers
+from swarms import models
+from swarms import structs
+from swarms import swarms
+from swarms.swarms.orchestrate import Orchestrator
+from swarms import agents
+from swarms.logo import logo
 print(logo)
---
+++ swarms/tools/edge_gpt.py
@@ -0,0 +1,9 @@
+from swarms.tools.tool import BaseTool
+
+class EdgeGPTTool(BaseTool):
+    def __init__(self, model, name="EdgeGPTTool", description="Tool that uses EdgeGPTModel to generate responses"):
+        super().__init__(name=name, description=description)
+        self.model = model
+
+    def _run(self, prompt):
+        return self.model.__call__(prompt)
