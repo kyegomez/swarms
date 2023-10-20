b79c748e34296485f84b7d350eacfc9247427344
Kye <kye@apacmediasolutions.com>
2023-10-20 01:17:19 -0400

    edgegpt clean up

--- swarms/models/__init__.py
+++ swarms/models/__init__.py
@@ -1,11 +1,12 @@
+# LLMs
 from swarms.models.anthropic import Anthropic
 from swarms.models.petals import Petals
 from swarms.models.mistral import Mistral
 from swarms.models.openai_models import OpenAI, AzureOpenAI, OpenAIChat
+from swarms.models.zephyr import Zephyr
 
 
 # MultiModal Models
 from swarms.models.idefics import Idefics
 from swarms.models.kosmos_two import Kosmos
 from swarms.models.vilt import Vilt
-from swarms.models.zephyr import Zephyr
