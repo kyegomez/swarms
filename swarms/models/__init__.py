7f690af7217943698dee3722df542e6a3a09af44
Kye <kye@apacmediasolutions.com>
2023-10-20 00:40:50 -0400

    kosmos, vilt, idefics,
    Former-commit-id: acb84370819e0feedbab2575a35ca70f4d3f134b

--- swarms/models/__init__.py
+++ swarms/models/__init__.py
@@ -2,4 +2,10 @@ from swarms.models.anthropic import Anthropic
 from swarms.models.petals import Petals
 from swarms.models.mistral import Mistral
 from swarms.models.openai_models import OpenAI, AzureOpenAI, OpenAIChat
-# from swarms.models.fuyu import Fuyu
+
+
+
+# MultiModal Models
+from swarms.models.idefics import Idefics
+from swarms.models.kosmos_two import Kosmos
+from swarms.models.vilt import Vilt
