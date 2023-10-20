975d35497ba647eae5927ed04a6c0bad90a1a64f
Zack <zack@zackbradshaw.com>
2023-10-19 23:43:01 -0500

    feat: Add bing tools and example
    Former-commit-id: c116aa8f1dcc679e907ce19afdc4ee8af176d0ce

---
+++ bingchat.py
@@ -0,0 +1,20 @@
+from swarms.models.bing_chat import EdgeGPTModel
+from swarms.workers.worker import Worker
+from swarms.tools.tool.bing_chat import EdgeGPTTool 
+
+# Initialize the EdgeGPTModel
+edgegpt = EdgeGPTModel(cookies_path="./path/to/cookies.json")
+
+# Initialize the custom tool
+edgegpt_tool = EdgeGPTTool(edgegpt)
+
+# Initialize the Worker with the custom tool
+worker = Worker(
+    ai_name="EdgeGPT Worker",
+    external_tools=[edgegpt_tool],
+)
+
+# Use the worker to process a task
+task = "Hello, my name is ChatGPT"
+response = worker.run(task)
+print(response)
