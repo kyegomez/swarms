38dd390e08aafccf12b95ac3c5cf98e9f1ce1f0c
Kye <kye@apacmediasolutions.com>
2023-10-20 13:06:11 -0400

    bioclip

--- swarms/models/nougat.py
+++ swarms/models/nougat.py
@@ -28,8 +28,9 @@ class Nougat:
     >>> nougat = Nougat()
     >>> nougat("path/to/image.png")
 
-    
+
     """
+
     def __init__(
         self,
         model_name_or_path="facebook/nougat-base",
