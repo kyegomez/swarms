0ac5b549310189275a9296cbe22ad05f3c99ac43
Zack <zack@zackbradshaw.com>
2023-10-24 18:00:42 -0500

    feat: add mythgen, gradio bot

--- apps/MythGen/main.py
+++ apps/MythGen/main.py
@@ -3,26 +3,18 @@ import os
 import dotenv
 import logging
 import gradio as gr
-from dalle3 import Dalle
-from bing_chat import BingChat
+from BingImageCreator import ImageGen
+from swarms.models.bing_chat import BingChat
 
 # from swarms.models.bingchat import BingChat  
-
 dotenv.load_dotenv(".env")
 
 # Initialize the EdgeGPTModel
-openai_api_key = os.getenv("OPENAI_API_KEY")
-model = BingChat(cookie_path = "./cookies.json")
 cookie = os.environ.get("BING_COOKIE")
 auth = os.environ.get("AUTH_COOKIE")
-
-
+model = BingChat(cookies_path="./cookies.json", bing_cookie="BING_COOKIE",auth_cookie="AUTH_COOKIE")
 
 response = model("Generate")
-
-# Initialize DALLE3 API
-cookie = os.getenv("DALLE_COOKIE")
-dalle = Dalle(cookie)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -31,14 +23,14 @@ latest_caption = ""
 standard_suffix = ""
 storyboard = []
 
-def generate_images_with_dalle(caption):
-    model.create_img(auth_cookie=cookie,auth_cookie_SRCHHPGUSR=auth,prompt=caption)
-    urls = dalle.get_urls()
-    return urls
+def generate_images_with_bingchat(caption):
+    img_path = model.create_img(caption)
+    img_urls = model.images(caption)
+    return img_urls
 
 def generate_single_caption(text):
     prompt = f"A comic about {text}."
-    response = model(prompt)
+    response = model(text)
     return response
 
 def interpret_text_with_gpt(text, suffix):
@@ -54,14 +46,14 @@ def gradio_interface(text=None, next_button_clicked=False):
         standard_suffix = create_standard_suffix(text)
     
     if next_button_clicked:
-        new_caption = interpret_text_with_gpt(latest_caption, standard_suffix)
-        new_urls = generate_images_with_dalle(new_caption)
+        new_caption = generate_single_caption(latest_caption + " " + standard_suffix)
+        new_urls = generate_images_with_bingchat(new_caption)
         latest_caption = new_caption
         storyboard.append((new_urls, new_caption))
         
     elif text:
-        caption = interpret_text_with_gpt(text, standard_suffix)
-        comic_panel_urls = generate_images_with_dalle(caption)
+        caption = generate_single_caption(text + " " + standard_suffix)
+        comic_panel_urls = generate_images_with_bingchat(caption)
         latest_caption = caption
         storyboard.append((comic_panel_urls, caption))
 
