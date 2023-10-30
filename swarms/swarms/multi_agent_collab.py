4dc18ba55b7d43d29bec160dc3b5f00fd4b9dda4
Kye <kye@apacmediasolutions.com>
2023-10-30 18:31:06 -0400

    clean up of misc items

--- swarms/swarms/multi_agent_collab.py
+++ swarms/swarms/multi_agent_collab.py
@@ -3,8 +3,6 @@ import tenacity
 from langchain.output_parsers import RegexParser
 
 # utils
-
-
 class BidOutputParser(RegexParser):
     def get_format_instructions(self) -> str:
         return "Your response should be an integrater delimited by angled brackets like this: <int>"
