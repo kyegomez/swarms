1b0cb87c737aa9e748f0132dd09ff9a714b4faf0
Kye <kye@apacmediasolutions.com>
2023-11-03 15:55:41 -0400

    no stream

--- swarms/agents/__init__.py
+++ swarms/agents/__init__.py
@@ -1,7 +1,7 @@
 from swarms.agents.omni_modal_agent import OmniModalAgent
 from swarms.agents.hf_agents import HFAgent
 from swarms.agents.message import Message
-from swarms.agents.stream_response import stream
+# from swarms.agents.stream_response import stream
 from swarms.agents.base import AbstractAgent
 from swarms.agents.registry import Registry
 from swarms.agents.idea_to_image_agent import Idea2Image
@@ -14,7 +14,6 @@ __all__ = [
     "OmniModalAgent",
     "HFAgent",
     "Message",
-    "stream",
     "AbstractAgent",
     "Registry",
     "Idea2Image",
