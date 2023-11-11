340000acf5249e19f0f4cf5a06bc9800ec8f80ae
Kye <kye@apacmediasolutions.com>
2023-11-11 17:43:51 -0500

    remove fuyu
    Former-commit-id: f77206896f34732d1d39288ccf84a2b41cbd2cff

--- swarms/models/__init__.py
+++ swarms/models/__init__.py
@@ -2,7 +2,6 @@
 from swarms.models.anthropic import Anthropic
 from swarms.models.petals import Petals
 from swarms.models.mistral import Mistral
-
 from swarms.models.openai_models import OpenAI, AzureOpenAI, OpenAIChat
 from swarms.models.zephyr import Zephyr
 from swarms.models.biogpt import BioGPT
@@ -16,10 +15,9 @@ from swarms.models.kosmos_two import Kosmos
 from swarms.models.vilt import Vilt
 from swarms.models.nougat import Nougat
 from swarms.models.layoutlm_document_qa import LayoutLMDocumentQA
-
-# from swarms.models.gpt4v import GPT4Vision
-# from swarms.models.dalle3 import Dalle3
-# from swarms.models.distilled_whisperx import DistilWhisperModel
+from swarms.models.gpt4v import GPT4Vision
+from swarms.models.dalle3 import Dalle3
+from swarms.models.distilled_whisperx import DistilWhisperModel
 # from swarms.models.fuyu import Fuyu  # Not working, wait until they update
 
 import sys
@@ -46,5 +44,5 @@ __all__ = [
     "WizardLLMStoryTeller",
     "GPT4Vision",
     "Dalle3",
-    "Fuyu",
+    # "Fuyu",
 ]
