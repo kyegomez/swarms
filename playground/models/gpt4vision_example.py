a85d956b5c7d7d532a3004df14a1ae069b89fd99
Kye <kye@apacmediasolutions.com>
2023-11-11 20:07:25 -0500

    clean
    Former-commit-id: 0d0e5e5dc31d262611e2bfd87e6ba52491b6be70

--- playground/models/gpt4vision_example.py
+++
@@ -1,7 +0,0 @@
-from swarms.models.gpt4v import GPT4Vision
-
-gpt4vision = GPT4Vision(api_key="")
-task = "What is the following image about?"
-img = "https://cdn.openai.com/dall-e/encoded/feats/feats_01J9J5ZKJZJY9.png"
-
-answer = gpt4vision.run(task, img)
