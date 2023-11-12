2669518c2b58e7d96a34afb3de74f45fcfca5a65
Zack <zack@zackbradshaw.com>
2023-11-11 18:54:29 -0600

    fix: Merge error
    Former-commit-id: 40432b1e5084c97f40a76f51e052c9a80eed91ca

--- swarms/utils/__init__.py
+++ swarms/utils/__init__.py
@@ -2,6 +2,7 @@ from swarms.utils.display_markdown import display_markdown_message
 from swarms.utils.futures import execute_futures_dict
 from swarms.utils.code_interpreter import SubprocessCodeInterpreter
 from swarms.utils.parse_code import extract_code_in_backticks_in_string
+from swarms.utils.tool_logging import get_logger
 
 __all__ = [
     "display_markdown_message",
