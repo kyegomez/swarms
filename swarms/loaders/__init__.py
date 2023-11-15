2d05a09e1ceb546664602b7aadcb3c92d046c3a4
Kye <kye@apacmediasolutions.com>
2023-11-15 16:56:37 -0500

    [FEAT][Playgrounds tests + no orchestrator
    Former-commit-id: ad1aecd09bc05e94bf8aa3f3d705076263a417bc

--- swarms/loaders/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Data Loaders for APPS
-
-
-TODO: Clean up all the llama index stuff, remake the logic from scratch
-
-"""
--- swarms/swarms/__init__.py
+++ swarms/swarms/__init__.py
@@ -1,6 +1,6 @@
 from swarms.swarms.dialogue_simulator import DialogueSimulator
 from swarms.swarms.autoscaler import AutoScaler
-from swarms.swarms.orchestrate import Orchestrator
+# from swarms.swarms.orchestrate import Orchestrator
 from swarms.swarms.god_mode import GodMode
 from swarms.swarms.simple_swarm import SimpleSwarm
 from swarms.swarms.multi_agent_debate import MultiAgentDebate, select_speaker
@@ -8,7 +8,7 @@ from swarms.swarms.multi_agent_debate import MultiAgentDebate, select_speaker
 __all__ = [
     "DialogueSimulator",
     "AutoScaler",
-    "Orchestrator",
+    # "Orchestrator",
     "GodMode",
     "SimpleSwarm",
     "MultiAgentDebate",
