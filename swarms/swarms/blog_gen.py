57023a9cdc6f7520aef2c274da5b48d26d079480
pliny <133052465+elder-plinius@users.noreply.github.com>
2023-11-19 18:33:20 -0800

    Update blog_gen.py

--- swarms/swarms/blog_gen.py
+++ swarms/swarms/blog_gen.py
@@ -1,7 +1,7 @@
 import os
 from termcolor import colored
 from swarms.models import OpenAIChat
-from swarms.models.AutoTemp import AutoTemp
+from swarms.models.autotemp import AutoTemp
 from swarms.structs import SequentialWorkflow
 
 
