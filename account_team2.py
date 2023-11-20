b8c9ab04fe7f510c7c99da29f9d26270fe31ecb0
Kye <kye@apacmediasolutions.com>
2023-11-19 22:29:56 -0800

    CLEAN UP: Structs folder of old workflow.py and nonlinearworkflow.py

--- account_team2.py
+++ account_team2.py
@@ -4,7 +4,6 @@ from swarms.models import Anthropic, OpenAIChat
 from swarms.prompts.accountant_swarm_prompts import (
     DECISION_MAKING_PROMPT,
     DOC_ANALYZER_AGENT_PROMPT,
-    FRAUD_DETECTION_AGENT_PROMPT,
     SUMMARY_GENERATOR_AGENT_PROMPT,
 )
 from swarms.structs import Flow
@@ -61,16 +60,13 @@ print(pdf_text)
 fraud_detection_agent_output = doc_analyzer_agent.run(
     f"{fraud_detection_instructions}: {pdf_text}"
 )
-# print(fraud_detection_agent_output)
 
 # Generate an actionable summary of the document
 summary_agent_output = summary_generator_agent.run(
     f"{summary_agent_instructions}: {fraud_detection_agent_output}"
 )
-# print(summary_agent_output)
 
 # Provide decision making support to the accountant
 decision_making_support_agent_output = decision_making_support_agent.run(
     f"{decision_making_support_agent_instructions}: {summary_agent_output}"
 )
-# print(decision_making_support_agent_output)
