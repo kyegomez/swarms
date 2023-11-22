13b4f27fb886897c8c340ffa1fc49f920d961b9f
Kye <kye@apacmediasolutions.com>
2023-11-21 22:37:02 -0800

    structs __init

--- swarms/structs/__init__.py
+++ swarms/structs/__init__.py
@@ -1,5 +1,3 @@
-# from swarms.structs.workflow import Workflow
-# from swarms.structs.task import Task
 from swarms.structs.flow import Flow
 from swarms.structs.sequential_workflow import SequentialWorkflow
 from swarms.structs.autoscaler import AutoScaler
