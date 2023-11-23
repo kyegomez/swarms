4ae59df890840017fb7e564ac63a15729976cd2e
Kye <kye@apacmediasolutions.com>
2023-11-23 15:01:14 -0800

    tools fix, parse docs, inject tools docs into prompts, and attempt to execute tools, display markdown

--- example.py
+++ example.py
@@ -1,37 +1,15 @@
 from swarms.models import OpenAIChat
 from swarms.structs import Flow
 
-# Initialize the language model, this model can be swapped out with Anthropic, ETC, Huggingface Models like Mistral, ETC
+# Initialize the language model
 llm = OpenAIChat(
-    # model_name="gpt-4"
-    # openai_api_key=api_key,
     temperature=0.5,
-    # max_tokens=100,
 )
 
 
 ## Initialize the workflow
-flow = Flow(
-    llm=llm,
-    max_loops=2,
-    dashboard=True,
-    # tools=[search_api]
-    # stopping_condition=None,  # You can define a stopping condition as needed.
-    # loop_interval=1,
-    # retry_attempts=3,
-    # retry_interval=1,
-    # interactive=False,  # Set to 'True' for interactive mode.
-    # dynamic_temperature=False,  # Set to 'True' for dynamic temperature handling.
-)
+flow = Flow(llm=llm, max_loops=1, dashboard=True)
 
-# out = flow.load_state("flow_state.json")
-# temp = flow.dynamic_temperature()
-# filter = flow.add_response_filter("Trump")
-out = flow.run(
-    "Generate a 10,000 word blog on mental clarity and the benefits of meditation."
-)
-# out = flow.validate_response(out)
-# out = flow.analyze_feedback(out)
-# out = flow.print_history_and_memory()
-# # out = flow.save_state("flow_state.json")
-# print(out)
+# Run the workflow on a task
+out = flow.run("Generate a 10,000 word blog on health and wellness.")
+
