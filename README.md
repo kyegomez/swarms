9390efb8aa3ed9aa7db3b21d669e3c8e6ab733af
Kye <kye@apacmediasolutions.com>
2023-11-24 22:05:49 -0800

    gpt4vision features and api

--- README.md
+++ README.md
@@ -119,6 +119,32 @@ for task in workflow.tasks:
 
 ```
 
+## `Multi Modal Autonomous Agents`
+- Run the flow with multiple modalities useful for various real-world tasks in manufacturing, logistics, and health.
+
+```python
+from swarms.structs import Flow
+from swarms.models.gpt4_vision_api import GPT4VisionAPI
+
+# Initialize the llm
+llm = GPT4VisionAPI()
+
+task = "Analyze this image of an assembly line and identify any issues such as misaligned parts, defects, or deviations from the standard assembly process. IF there is anything unsafe in the image, explain why it is unsafe and how it could be improved."
+img = "assembly_line.jpg"
+
+## Initialize the workflow
+flow = Flow(
+    llm=llm,
+    max_loops=1,
+    dashboard=True,
+)
+
+# Run the flow
+flow.run(task=task, img=img)
+
+
+```
+
 ---
 
 # Features 🤖 
--- swarms/models/gpt4_vision_api.py
+++ swarms/models/gpt4_vision_api.py
@@ -1,8 +1,15 @@
+import asyncio
 import base64
+import concurrent.futures
+from termcolor import colored
+import json
 import os
+from concurrent.futures import ThreadPoolExecutor
+from typing import List, Tuple
+
+import aiohttp
 import requests
 from dotenv import load_dotenv
-import concurrent.futures
 
 # Load environment variables
 load_dotenv()
@@ -22,7 +29,7 @@ class GPT4VisionAPI:
     max_tokens : int
         The maximum number of tokens to generate. Defaults to 300.
 
-    
+
     Methods
     -------
     encode_image(img: str)
@@ -43,15 +50,28 @@ class GPT4VisionAPI:
 
     """
 
-    def __init__(self, openai_api_key: str = openai_api_key, max_tokens: str = 300):
+    def __init__(
+        self,
+        openai_api_key: str = openai_api_key,
+        model_name: str = "gpt-4-vision-preview",
+        max_workers: int = 10,
+        max_tokens: str = 300,
+        openai_proxy: str = "https://api.openai.com/v1/chat/completions",
+    ):
         super().__init__()
         self.openai_api_key = openai_api_key
+        self.model_name = model_name
+        self.max_workers = max_workers
         self.max_tokens = max_tokens
+        self.openai_proxy = openai_proxy
 
     def encode_image(self, img: str):
         """Encode image to base64."""
         with open(img, "rb") as image_file:
             return base64.b64encode(image_file.read()).decode("utf-8")
+
+    def download_img_then_encode(self, img: str):
+        """Download image from URL then encode image to base64 using requests"""
 
     # Function to handle vision tasks
     def run(self, task: str, img: str):
@@ -63,7 +83,7 @@ class GPT4VisionAPI:
                 "Authorization": f"Bearer {openai_api_key}",
             }
             payload = {
-                "model": "gpt-4-vision-preview",
+                "model": self.model_name,
                 "messages": [
                     {
                         "role": "user",
@@ -125,7 +145,7 @@ class GPT4VisionAPI:
                 "max_tokens": self.max_tokens,
             }
             response = requests.post(
-                "https://api.openai.com/v1/chat/completions",
+                self.openai_proxy,
                 headers=headers,
                 json=payload,
             )
@@ -138,3 +158,135 @@ class GPT4VisionAPI:
             raise error
         # Function to handle vision tasks
 
+    def run_many(
+        self,
+        tasks: List[str],
+        imgs: List[str],
+    ):
+        """
+        Run the model on multiple tasks and images all at once using concurrent
+
+        """
+        # Instantiate the thread pool executor
+        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+            results = executor.map(self.run, tasks, imgs)
+
+        # Print the results for debugging
+        for result in results:
+            print(result)
+
+        return list(results)
+
+    async def arun(
+        self,
+        task: str,
+        img: str,
+    ):
+        """
+        Asynchronously run the model
+
+        Overview:
+        ---------
+        This method is used to asynchronously run the model. It is used to run the model
+        on a single task and image.
+
+        Parameters:
+        ----------
+        task : str
+            The task to run the model on.
+        img : str
+            The image to run the task on
+
+        """
+        try:
+            base64_image = self.encode_image(img)
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {openai_api_key}",
+            }
+            payload = {
+                "model": "gpt-4-vision-preview",
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": [
+                            {"type": "text", "text": task},
+                            {
+                                "type": "image_url",
+                                "image_url": {
+                                    "url": (
+                                        f"data:image/jpeg;base64,{base64_image}"
+                                    )
+                                },
+                            },
+                        ],
+                    }
+                ],
+                "max_tokens": self.max_tokens,
+            }
+            async with aiohttp.ClientSession() as session:
+                async with session.post(
+                    self.openai_proxy, headers=headers, data=json.dumps(payload)
+                ) as response:
+                    out = await response.json()
+                    content = out["choices"][0]["message"]["content"]
+                    print(content)
+        except Exception as error:
+            print(f"Error with the request {error}")
+            raise error
+
+    def run_batch(self, tasks_images: List[Tuple[str, str]]) -> List[str]:
+        """Process a batch of tasks and images"""
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(self.run, task, img)
+                for task, img in tasks_images
+            ]
+            results = [future.result() for future in futures]
+        return results
+
+    async def run_batch_async(
+        self, tasks_images: List[Tuple[str, str]]
+    ) -> List[str]:
+        """Process a batch of tasks and images asynchronously"""
+        loop = asyncio.get_event_loop()
+        futures = [
+            loop.run_in_executor(None, self.run, task, img)
+            for task, img in tasks_images
+        ]
+        return await asyncio.gather(*futures)
+
+    async def run_batch_async_with_retries(
+        self, tasks_images: List[Tuple[str, str]]
+    ) -> List[str]:
+        """Process a batch of tasks and images asynchronously with retries"""
+        loop = asyncio.get_event_loop()
+        futures = [
+            loop.run_in_executor(None, self.run_with_retries, task, img)
+            for task, img in tasks_images
+        ]
+        return await asyncio.gather(*futures)
+
+    def health_check(self):
+        """Health check for the GPT4Vision model"""
+        try:
+            response = requests.get("https://api.openai.com/v1/engines")
+            return response.status_code == 200
+        except requests.RequestException as error:
+            print(f"Health check failed: {error}")
+            return False
+
+    def print_dashboard(self):
+        dashboard = print(
+            colored(
+                f"""
+            GPT4Vision Dashboard
+            -------------------
+            Model: {self.model_name}
+            Max Workers: {self.max_workers}
+            OpenAIProxy: {self.openai_proxy}
+            """,
+                "green",
+            )
+        )
+        return dashboard
