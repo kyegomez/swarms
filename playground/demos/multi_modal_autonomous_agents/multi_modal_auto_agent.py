950e62888bf51e2f0f11704d493c771737671f3e
Kye <kye@apacmediasolutions.com>
2023-11-24 17:35:47 -0800

    GPT4Vision + multimodal

---
+++ playground/demos/multi_modal_autonomous_agents/multi_modal_auto_agent.py
@@ -0,0 +1,17 @@
+from swarms.structs import Flow
+from swarms.models.gpt4_vision_api import GPT4VisionAPI
+
+
+llm = GPT4VisionAPI()
+
+task = "What is the color of the object?"
+img = "images/swarms.jpeg"
+
+## Initialize the workflow
+flow = Flow(
+    llm=llm,
+    max_loops="auto",
+    dashboard=True,
+)
+
+flow.run(task=task, img=img)
