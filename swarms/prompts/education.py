a12130cf54082f16ba7506272d31dd59cb74d944
pliny <133052465+elder-plinius@users.noreply.github.com>
2023-12-02 13:37:21 -0800

    Create education.py

---
+++ swarms/prompts/education.py
@@ -0,0 +1,37 @@
+user_preferences = {
+    "subjects": "AI Cognitive Architectures",
+    "learning_style": "Visual",
+    "challenge_level": "Moderate",
+}
+
+# Extracting individual preferences
+subjects = user_preferences["subjects"]
+learning_style = user_preferences["learning_style"]
+challenge_level = user_preferences["challenge_level"]
+
+
+# Curriculum Design Prompt
+CURRICULUM_DESIGN_PROMPT = f"""
+Develop a semester-long curriculum tailored to student interests in {subjects}. Focus on incorporating diverse teaching methods suitable for a {learning_style} learning style. 
+The curriculum should challenge students at a {challenge_level} level, integrating both theoretical knowledge and practical applications. Provide a detailed structure, including weekly topics, 
+key objectives, and essential resources needed.
+"""
+
+# Interactive Learning Session Prompt
+INTERACTIVE_LEARNING_PROMPT = f"""
+You are being given a curriculum from another agent, please design an interactive learning sessions for the {subjects} curriculum that cater to a {learning_style} learning style. Incorporate 
+engaging elements like gamification, interactive quizzes, and hands-on activities. The sessions should dynamically adjust to the student's engagement and understanding, emphasizing key concepts 
+and practical skills.
+"""
+
+# Sample Lesson Prompt
+SAMPLE_TEST_PROMPT = f"""
+Create a comprehensive sample test for the first week of the {subjects} curriculum, tailored for a {learning_style} learning style and at a {challenge_level} challenge level.
+"""
+
+# Image Generation for Education Prompt
+IMAGE_GENERATION_PROMPT = f"""
+Develop a stable diffusion prompt for an educational image/visual aid that align with the {subjects} curriculum, specifically designed to enhance understanding for students with a {learning_style} 
+learning style. This might include diagrams, infographics, and illustrative representations to simplify complex concepts. Ensure you output a 10/10 descriptive image generation prompt only.
+"""
+
