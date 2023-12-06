980643eb2f35adb3c52a9440474d86f2f5af2b04
Kye <kye@apacmediasolutions.com>
2023-12-06 15:23:33 -0800

    [__INIT__][CLEANUP]

--- swarms/swarms/__init__.py
+++ swarms/swarms/__init__.py
@@ -1,14 +1,10 @@
-from swarms.swarms.dialogue_simulator import DialogueSimulator
 from swarms.structs.autoscaler import AutoScaler
 from swarms.swarms.god_mode import GodMode
-from swarms.swarms.simple_swarm import SimpleSwarm
 from swarms.swarms.multi_agent_collab import MultiAgentCollaboration
 
 
 __all__ = [
-    "DialogueSimulator",
     "AutoScaler",
     "GodMode",
-    "SimpleSwarm",
     "MultiAgentCollaboration",
 ]
