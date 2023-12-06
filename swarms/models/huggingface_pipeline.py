ab927f965a9ba628848726024faede2a55baf779
Kye <kye@apacmediasolutions.com>
2023-12-05 17:24:26 -0800

    [TESTS for HuggingfacePipeline]

--- swarms/models/huggingface_pipeline.py
+++ swarms/models/huggingface_pipeline.py
@@ -14,8 +14,6 @@ if torch.cuda.is_available():
 class HuggingfacePipeline(AbstractLLM):
     """HuggingfacePipeline
 
-
-
     Args:
         AbstractLLM (AbstractLLM): [description]
         task (str, optional): [description]. Defaults to "text-generation".
@@ -30,26 +28,45 @@ class HuggingfacePipeline(AbstractLLM):
 
     def __init__(
         self,
-        task: str = "text-generation",
+        task_type: str = "text-generation",
         model_name: str = None,
         use_fp8: bool = False,
         *args,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
+        self.task_type = task_type
+        self.model_name = model_name
+        self.use_fp8 = use_fp8
+
+        if torch.cuda.is_available():
+            self.use_fp8 = True
+        else:
+            self.use_fp8 = False
+
         self.pipe = pipeline(
-            task, model_name, use_fp8=use_fp8 * args, **kwargs
+            task_type, model_name, use_fp8=use_fp8 * args, **kwargs
         )
 
     @abstractmethod
-    def run(self, task: str, *args, **kwargs):
+    def run(self, task: str, *args, **kwargs) -> str:
+        """Run the pipeline
+
+        Args:
+            task (str): [description]
+            *args: [description]
+            **kwargs: [description]
+
+        Returns:
+            _type_: _description_
+        """
         try:
             out = self.pipeline(task, *args, **kwargs)
             return out
-        except Exception as e:
+        except Exception as error:
             print(
                 colored(
-                    f"Error in {self.__class__.__name__} pipeline",
+                    f"Error in {self.__class__.__name__} pipeline: {error}",
                     "red",
                 )
             )
