3f29ecb10d914cbd6c967f4cfc5bdd534ac86511
Kye <kye@apacmediasolutions.com>
2023-12-11 17:11:45 -0800

    [CODE QUALITY][Refactor Migrations][LayoutLMDocumentQA]

--- swarms/models/layoutlm_document_qa.py
+++ swarms/models/layoutlm_document_qa.py
@@ -3,9 +3,10 @@ LayoutLMDocumentQA is a multimodal good for
 visual question answering on real world docs lik invoice, pdfs, etc
 """
 from transformers import pipeline
+from swarms.models.base_multimodal_model import BaseMultiModalModel
 
 
-class LayoutLMDocumentQA:
+class LayoutLMDocumentQA(BaseMultiModalModel):
     """
     LayoutLMDocumentQA for document question answering:
 
@@ -25,13 +26,24 @@ class LayoutLMDocumentQA:
         self,
         model_name: str = "impira/layoutlm-document-qa",
         task_type: str = "document-question-answering",
+        *args,
+        **kwargs,
     ):
+        super(LayoutLMDocumentQA, self).__init__(*args, **kwargs)
         self.model_name = model_name
         self.task_type = task_type
         self.pipeline = pipeline(task_type, model=model_name)
 
-    def __call__(self, task: str, img_path: str):
-        """Call for model"""
+    def __call__(self, task: str, img_path: str, *args, **kwargs):
+        """Call the LayoutLMDocumentQA model
+
+        Args:
+            task (str): _description_
+            img_path (str): _description_
+
+        Returns:
+            _type_: _description_
+        """
         out = self.pipeline(img_path, task)
         out = str(out)
         return out
--- swarms/models/multion.py
+++ swarms/models/multion.py
@@ -1,17 +1,18 @@
+from swarms.models.base_llm import AbstractLLM
+
+
 try:
     import multion
 
 except ImportError:
     raise ImportError(
         "Cannot import multion, please install 'pip install'"
-        
     )
 
-from swarms.models.base_llm import AbstractLLM
 
-class MultiOn(AbstractLLM): 
+class MultiOn(AbstractLLM):
     """
-    MultiOn is a wrapper for the Multion API.  
+    MultiOn is a wrapper for the Multion API.
 
     Args:
         **kwargs:
@@ -19,19 +20,41 @@ class MultiOn(AbstractLLM):
     Methods:
         run(self, task: str, url: str, *args, **kwargs)
 
-    Example:    
+    Example:
     >>> from swarms.models.multion import MultiOn
     >>> multion = MultiOn()
     >>> multion.run("Order chicken tendies", "https://www.google.com/")
     "Order chicken tendies. https://www.google.com/"
-    
+
     """
+
     def __init__(self, **kwargs):
         super(MultiOn, self).__init__(**kwargs)
 
     def run(self, task: str, url: str, *args, **kwargs) -> str:
+        """Run the multion model
+
+        Args:
+            task (str): _description_
+            url (str): _description_
+
+        Returns:
+            str: _description_
+        """
         response = multion.new_session({"input": task, "url": url})
         return response
-    def generate_summary(self, task: str, url: str, *args, **kwargs) -> str:
+
+    def generate_summary(
+        self, task: str, url: str, *args, **kwargs
+    ) -> str:
+        """Generate a summary from the multion model
+
+        Args:
+            task (str): _description_
+            url (str): _description_
+
+        Returns:
+            str: _description_
+        """
         response = multion.new_session({"input": task, "url": url})
         return response
