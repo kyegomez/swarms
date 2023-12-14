bc25bd9aa10ff60365664de9c5fc193b12dd4c56
pliny <133052465+elder-plinius@users.noreply.github.com>
2023-12-13 23:23:50 -0800

    Update swarm_daddy.py

--- playground/demos/swarm_daddy/swarm_daddy.py
+++ playground/demos/swarm_daddy/swarm_daddy.py
@@ -11,15 +11,12 @@ llm = OpenAIChat(model_name = "gpt-4", openai_api_key=api_key)
 
 user_idea = "screenplay writing team"
 
-
-#idea_analysis_agent = Agent(llm=llm, sop=sdsp.IDEA_ANALYSIS_AGENT_PROMPT, max_loops=1)
 role_identification_agent = Agent(llm=llm, sop=sdsp.AGENT_ROLE_IDENTIFICATION_AGENT_PROMPT, max_loops=1)
 agent_configuration_agent = Agent(llm=llm, sop=sdsp.AGENT_CONFIGURATION_AGENT_PROMPT, max_loops=1)
 swarm_assembly_agent = Agent(llm=llm, sop=sdsp.SWARM_ASSEMBLY_AGENT_PROMPT, max_loops=1)
 testing_optimization_agent = Agent(llm=llm, sop=sdsp.TESTING_OPTIMIZATION_AGENT_PROMPT, max_loops=1)
 
 # Process the user idea through each agent
-# idea_analysis_output = idea_analysis_agent.run(user_idea)
 role_identification_output = role_identification_agent.run(user_idea)
 agent_configuration_output = agent_configuration_agent.run(role_identification_output)
 swarm_assembly_output = swarm_assembly_agent.run(agent_configuration_output)
