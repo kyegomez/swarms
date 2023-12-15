795c8b46be3bdd8d7365f256ec72f8b936002e5d
Zack <zack@zackbradshaw.com>
2023-12-14 16:10:24 -0800

    chore: sync

--- playground/demos/multi_modal_autonomous_agents/multi_modal_auto_agent.py
+++ playground/demos/multi_modal_autonomous_agents/multi_modal_auto_agent.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from swarms.structs import Agent
 from swarms.models.gpt4_vision_api import GPT4VisionAPI
 
@@ -15,3 +16,35 @@ agent = Agent(
 )
 
 agent.run(task=task, img=img)
+=======
+from swarms.structs import Flow
+from swarms.models import Idefics
+
+# Multi Modality Auto Agent
+llm = Idefics(max_length=2000)
+
+task = "User: What is in this image? https://upload.wikimedia.org/wikipedia/commons/8/86/Id%C3%A9fix.JPG"
+
+## Initialize the workflow
+flow = Flow(
+    llm=llm,
+    max_loops=2,
+    dashboard=True,
+    # stopping_condition=None,  # You can define a stopping condition as needed.
+    # loop_interval=1,
+    # retry_attempts=3,
+    # retry_interval=1,
+    # interactive=False,  # Set to 'True' for interactive mode.
+    # dynamic_temperature=False,  # Set to 'True' for dynamic temperature handling.
+)
+
+# out = flow.load_state("flow_state.json")
+# temp = flow.dynamic_temperature()
+# filter = flow.add_response_filter("Trump")
+out = flow.run(task)
+# out = flow.validate_response(out)
+# out = flow.analyze_feedback(out)
+# out = flow.print_history_and_memory()
+# # out = flow.save_state("flow_state.json")
+# print(out)
+>>>>>>> fa52e094 (CLEAN UP: Flow and demo layouts)
