387084cef927ed9cbef041213797aa9d24213253
Kye <kye@apacmediasolutions.com>
2023-12-22 21:50:26 -0500

    [FEAT][WeaviateClient]

--- swarms/memory/__init__.py
+++ swarms/memory/__init__.py
@@ -1,5 +1,6 @@
 from swarms.memory.base_vectordb import VectorDatabase
 from swarms.memory.short_term_memory import ShortTermMemory
 from swarms.memory.sqlite import SQLiteDB
+from swarms.memory.weaviate_db import WeaviateClient
 
-__all__ = ["VectorDatabase", "ShortTermMemory", "SQLiteDB"]
+__all__ = ["VectorDatabase", "ShortTermMemory", "SQLiteDB", "WeaviateClient"]
