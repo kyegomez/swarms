a9b91d46533616b2e62c36fee1677fc81adeab02
Kye <kye@apacmediasolutions.com>
2023-12-23 18:23:02 -0500

    [FEAT][load_model_torch]

--- swarms/utils/__init__.py
+++ swarms/utils/__init__.py
@@ -6,15 +6,16 @@ from swarms.utils.parse_code import (
 from swarms.utils.pdf_to_text import pdf_to_text
 from swarms.utils.math_eval import math_eval
 from swarms.utils.llm_metrics_decorator import metrics_decorator
-
-# from swarms.utils.phoenix_handler import phoenix_trace_decorator
+from swarms.utils.device_checker_cuda import check_device
+from swarms.utils.load_model_torch import load_model_torch
 
 __all__ = [
     "display_markdown_message",
     "SubprocessCodeInterpreter",
     "extract_code_in_backticks_in_string",
     "pdf_to_text",
-    # "phoenix_trace_decorator",
     "math_eval",
     "metrics_decorator",
+    "check_device",
+    "load_model_torch",
 ]
