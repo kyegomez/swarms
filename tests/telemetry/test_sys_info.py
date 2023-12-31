552b7c10eac6c42d55655f5dd1343876d8fe0a95
evelynmitchell <efmphone@gmail.com>
2023-12-31 16:07:20 -0700

    typo, added docstrings

--- tests/telemetry/test_sys_info.py
+++ tests/telemetry/test_sys_info.py
@@ -1,8 +1,11 @@
-iimport pytest
+"""Tests for the sys_info module."""
+
+import pytest
 from unittest.mock import Mock
 from your_module import interpreter_info, system_info  # replace with your actual module name
 
 def test_interpreter_info(mocker):
+    """Test interpreter_info."""
     mocker.patch('subprocess.check_output', return_value='curl output')
     interpreter = Mock()
     interpreter.offline = True
@@ -21,6 +24,7 @@ def test_interpreter_info(mocker):
     assert 'curl output' in result
 
 def test_system_info(mocker):
+    """Test system_info."""
     mocker.patch('your_module.get_oi_version', return_value=('cmd_version', 'pkg_version'))  # replace with your actual module name
     mocker.patch('your_module.get_python_version', return_value='python_version')  # replace with your actual module name
     mocker.patch('your_module.get_pip_version', return_value='pip_version')  # replace with your actual module name
