38438c2266c141b2b8868e6f3be987c515e415ed
Kye <kye@apacmediasolutions.com>
2023-12-31 01:00:21 -0500

    [FEAT]][Vllm]

--- swarms/models/__init__.py
+++ swarms/models/__init__.py
@@ -9,8 +9,8 @@ from swarms.models.openai_models import (
     OpenAIChat,
 )  # noqa: E402
 
-# from swarms.models.vllm import vLLM  # noqa: E402
-# from swarms.models.zephyr import Zephyr  # noqa: E402
+from swarms.models.vllm import vLLM  # noqa: E402
+from swarms.models.zephyr import Zephyr  # noqa: E402
 from swarms.models.biogpt import BioGPT  # noqa: E402
 from swarms.models.huggingface import HuggingfaceLLM  # noqa: E402
 from swarms.models.wizard_storytelling import (
@@ -64,7 +64,7 @@ __all__ = [
     # "Dalle3",
     # "DistilWhisperModel",
     "GPT4VisionAPI",
-    # "vLLM",
+    "vLLM",
     "OpenAITTS",
     "Gemini",
     "Gigabind",
