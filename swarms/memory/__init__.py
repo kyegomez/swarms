4f39f4fe0ace20126d410c7e7e8ca0fc116f1bc9
Kye <kye@apacmediasolutions.com>
2024-01-28 20:00:47 -0500

    [CLEANUP]

--- swarms/memory/__init__.py
+++ swarms/memory/__init__.py
@@ -1,4 +1,5 @@
 from swarms.memory.base_vectordb import AbstractVectorDatabase
+from swarms.memory.base_db import AbstractDatabase
 from swarms.memory.short_term_memory import ShortTermMemory
 from swarms.memory.sqlite import SQLiteDB
 from swarms.memory.weaviate_db import WeaviateDB
@@ -6,6 +7,7 @@ from swarms.memory.visual_memory import VisualShortTermMemory
 
 __all__ = [
     "AbstractVectorDatabase",
+    "AbstractDatabase",
     "ShortTermMemory",
     "SQLiteDB",
     "WeaviateDB",
