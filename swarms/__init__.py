0e8b82677f13df3e87b30f76ebb9a5fdcb9135fc
evelynmitchell <efmphone@gmail.com>
2024-02-02 08:54:39 -0700

    add docstring, silence warnings swarms init

--- swarms/__init__.py
+++ swarms/__init__.py
@@ -1,15 +1,16 @@
+""" init file for swarms package. """
 # from swarms.telemetry.main import Telemetry  # noqa: E402, F403
 from swarms.telemetry.bootup import bootup  # noqa: E402, F403
 
 bootup()
 
-from swarms.agents import *  # noqa: E402, F403
-from swarms.structs import *  # noqa: E402, F403
-from swarms.models import *  # noqa: E402, F403
-from swarms.telemetry import *  # noqa: E402, F403
-from swarms.utils import *  # noqa: E402, F403
-from swarms.prompts import *  # noqa: E402, F403
-from swarms.tokenizers import *  # noqa: E402, F403
-from swarms.loaders import *  # noqa: E402, F403
-from swarms.artifacts import *  # noqa: E402, F403
-from swarms.chunkers import *  # noqa: E402, F403
+from swarms.agents import *  # noqa: E402, F403, C0413
+from swarms.structs import *  # noqa: E402, F403, C0413
+from swarms.models import *  # noqa: E402, F403,    C0413
+from swarms.telemetry import *  # noqa: E402, F403, C0413
+from swarms.utils import *  # noqa: E402, F403, C0413
+from swarms.prompts import *  # noqa: E402, F403, C0413
+from swarms.tokenizers import *  # noqa: E402, F403, C0413
+from swarms.loaders import *  # noqa: E402, F403, C0413
+from swarms.artifacts import *  # noqa: E402, F403, C0413
+from swarms.chunkers import *  # noqa: E402, F403, C0413
