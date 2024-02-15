145a1f94aa45cb55e9867d6901e2dd4c04d734f8
evelynmitchell <efmphone@gmail.com>
2024-02-15 14:42:38 -0700

    fix typo

--- playground/models/openai_model_example.py
+++ playground/models/openai_model_example.py
@@ -1,6 +1,6 @@
 from swarms.models.openai_models import OpenAIChat
 
-openai = OpenAIChat(openai_api_key="", openai_org_id="" verbose=False)
+openai = OpenAIChat(openai_api_key="", openai_org_id="", verbose=False)
 
 
 chat = openai("What are quantum fields?")
