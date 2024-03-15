fe4966c84912a2ed82b8d38aea28065edaf95d9d
Kye <kye@apacmediasolutions.com>
2024-03-15 00:14:48 -0700

    [CLEANUP]

--- swarms/models/__init__.py
+++ swarms/models/__init__.py
@@ -2,17 +2,8 @@ from swarms.models.anthropic import Anthropic  # noqa: E402
 from swarms.models.base_embedding_model import BaseEmbeddingModel
 from swarms.models.base_llm import AbstractLLM  # noqa: E402
 from swarms.models.base_multimodal_model import BaseMultiModalModel
-
-# noqa: E402
 from swarms.models.biogpt import BioGPT  # noqa: E402
 from swarms.models.clipq import CLIPQ  # noqa: E402
-
-# from swarms.models.dalle3 import Dalle3
-# from swarms.models.distilled_whisperx import DistilWhisperModel # noqa: E402
-# from swarms.models.whisperx_model import WhisperX  # noqa: E402
-# from swarms.models.kosmos_two import Kosmos  # noqa: E402
-# from swarms.models.cog_agent import CogAgent  # noqa: E402
-## Function calling models
 from swarms.models.fire_function import FireFunctionCaller
 from swarms.models.fuyu import Fuyu  # noqa: E402
 from swarms.models.gemini import Gemini  # noqa: E402
@@ -22,20 +13,11 @@ from swarms.models.huggingface import HuggingfaceLLM  # noqa: E402
 from swarms.models.idefics import Idefics  # noqa: E402
 from swarms.models.kosmos_two import Kosmos  # noqa: E402
 from swarms.models.layoutlm_document_qa import LayoutLMDocumentQA
-
-# noqa: E402
 from swarms.models.llava import LavaMultiModal  # noqa: E402
 from swarms.models.mistral import Mistral  # noqa: E402
 from swarms.models.mixtral import Mixtral  # noqa: E402
 from swarms.models.mpt import MPT7B  # noqa: E402
 from swarms.models.nougat import Nougat  # noqa: E402
-from swarms.models.openai_models import (
-    AzureOpenAI,
-    OpenAI,
-    OpenAIChat,
-)
-
-# noqa: E402
 from swarms.models.openai_tts import OpenAITTS  # noqa: E402
 from swarms.models.petals import Petals  # noqa: E402
 from swarms.models.qwen import QwenVLMultiModal  # noqa: E402
@@ -43,14 +25,7 @@ from swarms.models.roboflow_model import RoboflowMultiModal
 from swarms.models.sam_supervision import SegmentAnythingMarkGenerator
 from swarms.models.sampling_params import SamplingParams, SamplingType
 from swarms.models.timm import TimmModel  # noqa: E402
-
-# from swarms.models.modelscope_pipeline import ModelScopePipeline
-# from swarms.models.modelscope_llm import (
-#     ModelScopeAutoModel,
-# )  # noqa: E402
 from swarms.models.together import TogetherLLM  # noqa: E402
-
-############## Types
 from swarms.models.types import (  # noqa: E402
     AudioModality,
     ImageModality,
@@ -59,60 +34,49 @@ from swarms.models.types import (  # noqa: E402
     VideoModality,
 )
 from swarms.models.ultralytics_model import UltralyticsModel
-
-# noqa: E402
 from swarms.models.vilt import Vilt  # noqa: E402
 from swarms.models.wizard_storytelling import WizardLLMStoryTeller
-
-# noqa: E402
-# from swarms.models.vllm import vLLM  # noqa: E402
 from swarms.models.zephyr import Zephyr  # noqa: E402
 from swarms.models.zeroscope import ZeroscopeTTV  # noqa: E402
 
 __all__ = [
-    "AbstractLLM",
     "Anthropic",
-    "Petals",
-    "Mistral",
-    "OpenAI",
-    "AzureOpenAI",
-    "OpenAIChat",
-    "Zephyr",
-    "BaseMultiModalModel",
-    "Idefics",
-    "Vilt",
-    "Nougat",
-    "LayoutLMDocumentQA",
-    "BioGPT",
-    "HuggingfaceLLM",
-    "MPT7B",
-    "WizardLLMStoryTeller",
-    # "Dalle3",
-    # "DistilWhisperModel",
-    "GPT4VisionAPI",
-    # "vLLM",
-    "OpenAITTS",
-    "Gemini",
-    "Gigabind",
-    "Mixtral",
-    "ZeroscopeTTV",
-    "TextModality",
-    "ImageModality",
-    "AudioModality",
-    "VideoModality",
-    "MultimodalData",
-    "TogetherLLM",
-    "TimmModel",
-    "UltralyticsModel",
-    "LavaMultiModal",
-    "QwenVLMultiModal",
-    "CLIPQ",
-    "Kosmos",
-    "Fuyu",
+    "AbstractLLM",
     "BaseEmbeddingModel",
+    "BaseMultiModalModel",
+    "BioGPT",
+    "CLIPQ",
+    "FireFunctionCaller",
+    "Fuyu",
+    "Gigabind",
+    "GPT4VisionAPI",
+    "HuggingfaceLLM",
+    "Idefics",
+    "Kosmos",
+    "LavaMultiModal",
+    "LayoutLMDocumentQA",
+    "Mistral",
+    "Mixtral",
+    "MPT7B",
+    "Nougat",
+    "Gemini",
+    "OpenAITTS",
+    "Petals",
+    "QwenVLMultiModal",
     "RoboflowMultiModal",
     "SegmentAnythingMarkGenerator",
-    "SamplingType",
     "SamplingParams",
-    "FireFunctionCaller",
+    "SamplingType",
+    "TimmModel",
+    "TogetherLLM",
+    "UltralyticsModel",
+    "Vilt",
+    "WizardLLMStoryTeller",
+    "Zephyr",
+    "ZeroscopeTTV",
+    "AudioModality",
+    "ImageModality",
+    "MultimodalData",
+    "TextModality",
+    "VideoModality",
 ]
