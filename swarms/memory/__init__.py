f4c66926717892ff6318499b4024870b34d1b21a
Kye <kye@apacmediasolutions.com>
2024-03-26 10:08:58 -0700

    [CLEANUP]

--- swarms/memory/__init__.py
+++ swarms/memory/__init__.py
@@ -1,21 +1,17 @@
 from swarms.memory.action_subtask import ActionSubtaskEntry
 from swarms.memory.base_db import AbstractDatabase
 from swarms.memory.base_vectordb import AbstractVectorDatabase
-from swarms.memory.chroma_db import ChromaDB
 from swarms.memory.dict_internal_memory import DictInternalMemory
 from swarms.memory.dict_shared_memory import DictSharedMemory
 from swarms.memory.short_term_memory import ShortTermMemory
-from swarms.memory.sqlite import SQLiteDB
 from swarms.memory.visual_memory import VisualShortTermMemory
 
 __all__ = [
-    "AbstractVectorDatabase",
     "AbstractDatabase",
-    "ShortTermMemory",
-    "SQLiteDB",
-    "VisualShortTermMemory",
+    "AbstractVectorDatabase",
     "ActionSubtaskEntry",
-    "ChromaDB",
     "DictInternalMemory",
     "DictSharedMemory",
+    "ShortTermMemory",
+    "VisualShortTermMemory",
 ]
