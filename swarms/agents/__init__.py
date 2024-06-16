20a994b62fbce78055739ba723291bb8bd470c11
Kye Gomez <swarms_wd@Kyes-MacBook-Pro.local>
2024-06-16 14:59:23 -0700

    [CLEANUP]

--- swarms/agents/__init__.py
+++ swarms/agents/__init__.py
@@ -1,7 +1,5 @@
 from swarms.agents.agent_wrapper import agent_wrapper
 from swarms.agents.base import AbstractAgent
-from swarms.agents.omni_modal_agent import OmniModalAgent
-from swarms.agents.simple_agent import SimpleAgent
 from swarms.agents.stopping_conditions import (
     check_cancelled,
     check_complete,
