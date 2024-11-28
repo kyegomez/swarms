ba5d9117d5f780f23f0a7a90fd768a37303c3e1c
evelynmitchell <efmphone@gmail.com>
2024-11-27 19:40:53 -0700

    suppress unused import errors

--- swarms/__init__.py
+++ swarms/__init__.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 
 from swarms.telemetry.bootup import bootup  # noqa: E402, F403
-from swarms.telemetry.sentry_active import (
+from swarms.telemetry.sentry_active import (  # noqa: E402
     activate_sentry,
 )  # noqa: E402
 
@@ -18,11 +18,11 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
     executor.submit(bootup)
     executor.submit(activate_sentry)
 
-from swarms.agents import *  # noqa: E402, F403
-from swarms.artifacts import *  # noqa: E402, F403
-from swarms.prompts import *  # noqa: E402, F403
-from swarms.schemas import *  # noqa: E402, F403
-from swarms.structs import *  # noqa: E402, F403
-from swarms.telemetry import *  # noqa: E402, F403
-from swarms.tools import *  # noqa: E402, F403
-from swarms.utils import *  # noqa: E402, F403
+from swarms.agents import *  # noqa: E402, F401, F403
+from swarms.artifacts import *  # noqa: E402, F401, F403
+from swarms.prompts import *  # noqa: E402, F401 F403
+from swarms.schemas import *  # noqa: E402, F401, F403
+from swarms.structs import *  # noqa: E402, F401, F403
+from swarms.telemetry import *  # noqa: E402, F401, F403
+from swarms.tools import *  # noqa: E402, F401, F403
+from swarms.utils import *  # noqa: E402, F401, F403
