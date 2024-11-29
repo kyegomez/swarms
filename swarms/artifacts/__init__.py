b51840c12dc027984ab45edfb057b393f0e0196d
Your Name <you@example.com>
2024-11-29 11:04:22 -0800

    [REMOVE ARTIFACTS FROM SWARMS.ARTIFACTS]

--- swarms/artifacts/__init__.py
+++ swarms/artifacts/__init__.py
@@ -1,9 +1,6 @@
-from swarms.artifacts.base_artifact import BaseArtifact
-from swarms.artifacts.text_artifact import TextArtifact
+
 from swarms.artifacts.main_artifact import Artifact
 
 __all__ = [
-    "BaseArtifact",
-    "TextArtifact",
     "Artifact",
 ]
