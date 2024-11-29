b4945bbd0a4307310ef51e47358f25f44bc8a098
Your Name <you@example.com>
2024-11-29 11:45:08 -0800

    [CLEANUP]

--- no_llm_agent.py
+++ no_llm_agent.py
@@ -3,5 +3,7 @@ from swarms import Agent
 Agent(
     agent_name="Stock-Analysis-Agent",
     model_name="gpt-4o-mini",
-    max_loops=1,
+    max_loops="auto",
+    streaming_on=True,
+    interactive=True,
 ).run("What are 5 hft algorithms")
