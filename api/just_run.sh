334c198fdf2e8c4a6a4a261c35148366f1b57ef6
mike dupont <mike.dupont@introspector.local>
2024-12-18 13:58:33 -0500

    update just run

--- api/just_run.sh
+++ api/just_run.sh
@@ -8,40 +8,46 @@ export BRANCH="feature/ec2"
 export ROOT="" # empty
 export WORKSOURCE="${ROOT}/opt/swarms/api"
 
-    cd "${ROOT}/opt/swarms/" || exit 1 # "we need swarms"
-    git checkout --force  $BRANCH
-    git pull 
-    git log -2 --patch | head  -1000
-    cp "${WORKSOURCE}/boot_fast.sh" "${ROOT}/var/swarms/agent_workspace/boot_fast.sh"
-    mkdir -p "${ROOT}/var/swarms/logs"
-    chmod +x "${ROOT}/var/swarms/agent_workspace/boot_fast.sh"
-    chown -R swarms:swarms "${ROOT}/var/swarms/" "${ROOT}/home/swarms" "${ROOT}/opt/swarms"
+adduser --disabled-password --gecos "" swarms --home "${ROOT}/home/swarms"  || echo ignore
+git config --global --add safe.directory "${ROOT}/opt/swarms"
+git config --global --add safe.directory "${ROOT}/opt/swarms-memory"
 
-    # user install but do not start
-    su -c "bash -e -x ${ROOT}/var/swarms/agent_workspace/boot_fast.sh" swarms
+cd "${ROOT}/opt/swarms/" || exit 1 # "we need swarms"
+git checkout --force  $BRANCH
+git pull 
+git log -2 --patch | head  -1000
 
-    cd "${ROOT}/opt/swarms/" || exit 1 # "we need swarms"
-#    git fetch local 
-#    git stash
-    git checkout --force  $BRANCH
-    git pull # $BRANCH
-
-    mkdir -p "${ROOT}/var/run/swarms/secrets/"
-    mkdir -p "${ROOT}/home/swarms/.cache/huggingface/hub"
-    # aws ssm get-parameter     --name "swarms_openai_key" > /root/openaikey.txt
-    export OPENAI_KEY=`aws ssm get-parameter     --name "swarms_openai_key" | jq .Parameter.Value -r `
-    echo "OPENAI_KEY=${OPENAI_KEY}" > "${ROOT}/var/run/swarms/secrets/env"
-    
-    ## append new homedir
-    echo "HF_HOME=${ROOT}/home/swarms/.cache/huggingface/hub" >> "${ROOT}/var/run/swarms/secrets/env"
-    echo "HOME=${ROOT}/home/swarms" >> "${ROOT}/var/run/swarms/secrets/env"
-    # attempt to move the workspace
-    echo 'WORKSPACE_DIR=${STATE_DIRECTORY}' >> "${ROOT}/var/run/swarms/secrets/env"
-    #EnvironmentFile=ROOT/var/run/swarms/secrets/env
-    #ExecStart=ROOT/var/run/uvicorn/env/bin/uvicorn \
-	#	--uds ROOT/run/uvicorn/uvicorn-swarms-api.sock \
+mkdir -p "${ROOT}/var/swarms/agent_workspace/"
+mkdir -p "${ROOT}/home/swarms"
 
 
+cd "${ROOT}/opt/swarms/" || exit 1 # "we need swarms"
+git checkout --force  $BRANCH
+git pull 
+git log -2 --patch | head  -1000
+cp "${WORKSOURCE}/boot_fast.sh" "${ROOT}/var/swarms/agent_workspace/boot_fast.sh"
+mkdir -p "${ROOT}/var/swarms/logs"
+chmod +x "${ROOT}/var/swarms/agent_workspace/boot_fast.sh"
+chown -R swarms:swarms "${ROOT}/var/swarms/" "${ROOT}/home/swarms" "${ROOT}/opt/swarms"
+
+# user install but do not start
+su -c "bash -e -x ${ROOT}/var/swarms/agent_workspace/boot_fast.sh" swarms
+
+cd "${ROOT}/opt/swarms/" || exit 1 # "we need swarms"
+git checkout --force  $BRANCH
+git pull # $BRANCH
+
+mkdir -p "${ROOT}/var/run/swarms/secrets/"
+mkdir -p "${ROOT}/home/swarms/.cache/huggingface/hub"
+# aws ssm get-parameter     --name "swarms_openai_key" > /root/openaikey.txt
+export OPENAI_KEY=`aws ssm get-parameter     --name "swarms_openai_key" | jq .Parameter.Value -r `
+echo "OPENAI_KEY=${OPENAI_KEY}" > "${ROOT}/var/run/swarms/secrets/env"
+
+## append new homedir
+echo "HF_HOME=${ROOT}/home/swarms/.cache/huggingface/hub" >> "${ROOT}/var/run/swarms/secrets/env"
+echo "HOME=${ROOT}/home/swarms" >> "${ROOT}/var/run/swarms/secrets/env"
+# attempt to move the workspace
+echo 'WORKSPACE_DIR=${STATE_DIRECTORY}' >> "${ROOT}/var/run/swarms/secrets/env"
 
 chown -R swarms:swarms ${ROOT}/var/run/swarms/
 mkdir -p ${ROOT}/opt/swarms/api/agent_workspace/try_except_wrapper/
