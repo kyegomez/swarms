e06652ed8b016d9f5f3d7288c018976da26f0a89
Kye Gomez <kye@swarms.world>
2025-01-22 03:57:53 -0500

    [UPDATE]

--- dex_screener.py
+++ dex_screener.py
@@ -1,30 +1,23 @@
 from swarms import Agent
-from swarms.prompts.finance_agent_sys_prompt import (
-    FINANCIAL_AGENT_SYS_PROMPT,
-)
-from swarms_tools import (
-    yahoo_finance_api,
+
+from swarms_tools.finance.dex_screener import (
+    fetch_dex_screener_profiles,
 )
 
 # Initialize the agent
 agent = Agent(
     agent_name="Financial-Analysis-Agent",
     agent_description="Personal finance advisor agent",
-    system_prompt=FINANCIAL_AGENT_SYS_PROMPT,
     max_loops=1,
     model_name="gpt-4o",
     dynamic_temperature_enabled=True,
     user_name="swarms_corp",
-    retry_attempts=3,
-    context_length=8192,
     return_step_meta=False,
     output_type="str",  # "json", "dict", "csv" OR "string" "yaml" and
     auto_generate_prompt=False,  # Auto generate prompt for the agent based on name, description, and system prompt, task
-    max_tokens=4000,  # max output tokens
-    saved_state_path="agent_00.json",
     interactive=False,
-    tools=[yahoo_finance_api],
 )
 
-agent.run("Analyze the latest metrics for nvidia")
-# Less than 30 lines of code....
+token_profiles = fetch_dex_screener_profiles()
+prompt = f"Using data from DexScreener, analyze the latest tokens and provide a detailed analysis with top 5 tokens based on their potential, considering both their profiles and recent boosts. The token profiles are sourced from DexScreener's token profiles API, while the token boosts are sourced from DexScreener's latest token boosts API. {str(token_profiles)}"
+agent.run(prompt)
