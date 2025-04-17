389567d2e7b4e833f3413f91eca535d554b8b8f4
Kye Gomez <kye@swarms.world>
2025-04-17 07:28:18 -0700

    swarms agent in rust docs

--- example.py
+++ example.py
@@ -2,10 +2,6 @@ from swarms.structs.agent import Agent
 from swarms.prompts.finance_agent_sys_prompt import (
     FINANCIAL_AGENT_SYS_PROMPT,
 )
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 # Initialize the agent
 agent = Agent(
@@ -13,20 +9,9 @@ agent = Agent(
     agent_description="Personal finance advisor agent",
     system_prompt=FINANCIAL_AGENT_SYS_PROMPT,
     max_loops="auto",
-    model_name="gpt-4o-mini",
+    model_name="openai/o4-mini",
     dynamic_temperature_enabled=True,
-    user_name="swarms_corp",
-    retry_attempts=3,
-    context_length=8192,
-    return_step_meta=False,
-    output_type="all",  # "json", "dict", "csv" OR "string" "yaml" and
-    auto_generate_prompt=False,  # Auto generate prompt for the agent based on name, description, and system prompt, task
-    max_tokens=4000,  # max output tokens
-    saved_state_path="agent_00.json",
     interactive=True,
-    role="director",
 )
 
-agent.run(
-    "Conduct an analysis of the best real undervalued ETFs. Think for 2 loops internally"
-)
+agent.run("Conduct an analysis of the best real undervalued ETFs")
