8b9e630e4485d07221e686bc8dd3bedc2ce868d4
Kye Gomez <kye@swarms.world>
2025-05-05 17:04:00 -0700

    cleanup

--- swarms/schemas/__init__.py
+++ swarms/schemas/__init__.py
@@ -1,10 +1,7 @@
 from swarms.schemas.agent_step_schemas import Step, ManySteps
 
-from swarms.schemas.agent_input_schema import AgentSchema
-
 
 __all__ = [
     "Step",
     "ManySteps",
-    "AgentSchema",
 ]
