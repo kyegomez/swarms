fd6553ecec1873ff557759c0b4e947da0982df37
ayaangazali <ayaangazali.work@gmail.com>
2026-09-14 20:06:32 -0700

    [FIX][deep_discussion,multi_agent_debates][Type the optional agents parameter as Optional]

--- swarms/structs/multi_agent_debates.py
+++ swarms/structs/multi_agent_debates.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from swarms.structs.agent import Agent
 from swarms.structs.conversation import Conversation
@@ -16,7 +16,7 @@ class OneOnOneDebate:
     def __init__(
         self,
         max_loops: int = 1,
-        agents: list[Agent] = None,
+        agents: Optional[list[Agent]] = None,
         img: str = None,
         output_type: str = "str-all-except-first",
     ):
